2e20a45058fd134426c9978cb21dbf47c6f63661
Brian Honohan <brian.honohan@charitywater.org>
2017-10-09 23:22:58 -0400

    p5js - Ecosystem - Adjust the water factor based on non-linear factor
    Testing showed that the water percentage requested varied greatly
    from the output, due to volatility in the Perlin noise function
    and what seed was used.
    
    I built up this map through some ad hoc empirical testing
    ... though it only works on the default settings.
    ... once the 'noise.falloff' is changed, this mapping is invalid.
    
    This is pretty hacky ...
    
    Another approach I consider was raising / lowering sea-level after the fact.
    ... but this falls short if we want to pan the map to new areas, it will
    fall victim to the same issues of volatility in the Perlin noise function.
    
    I feel this is good enough for now.
    
    Later might consider generating "continents" or regions first, and then
    using noise to alter the terrain.

--- p5js/ecosystem/classes/ecosystem.js
+++ p5js/ecosystem/classes/ecosystem.js
@@ -3,6 +3,7 @@ class Ecosystem {
     this.sizeAndPosition = p_xSizeAndPos;
     this.settings = {}
     Object.assign(this.settings, this.getDefaultOptions(), p_xOptions);
+    this.determineMagicWaterPercentageFactor();
 
     this.grid = new CellGrid(this.sizeAndPosition, this, this.settings.cellWidth);
   }
@@ -11,8 +12,43 @@ class Ecosystem {
     return this.settings.scale;
   }
 
+  determineMagicWaterPercentageFactor(){
+    var mapRequestToFactor = [
+          [0.0 , 0]
+        , [0.05, 0.1 ]
+        , [0.10, 0.2 ]
+        , [0.25, 0.3 ]
+        , [0.30, 0.33]
+        , [0.45, 0.35]
+        , [0.60, 0.4 ]
+        , [0.70, 0.5 ]
+        , [0.80, 0.52]
+        , [0.90, 0.55]
+        , [0.95, 0.6 ]
+        , [1.0,  0.8 ]
+      ];
+
+    if (this.settings.percentWater >= 1) {
+      this.adjustedWaterPercent = 1;
+      return;
+    } else if(this.settings.percentWater <= 0) {
+      this.adjustedWaterPercent = 0;
+      return;
+    }
+
+    for (var i = 0; i < mapRequestToFactor.length; i++){
+      let mapping = mapRequestToFactor[i];
+      let nextMapping = mapRequestToFactor[i+1];
+      if (nextMapping[0] > this.settings.percentWater){
+        this.adjustedWaterPercent = mapping[1];
+        break;
+      }
+    }
+  }
+
+
   getPercentWater(){
-    return this.settings.percentWater;
+    return this.adjustedWaterPercent;
   }
 
   getDefaultOptions(){
