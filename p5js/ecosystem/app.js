95f78f8b0f6dcfb8d831d3f267cf5c15f957e766
Brian Honohan <brian.honohan@gmail.com>
2018-08-25 15:28:40 -0400

    p5js - Ecosystem - Re-use existing JS Util Functions
    Reduce code duplication and code clutter.

--- p5js/ecosystem/app.js
+++ p5js/ecosystem/app.js
@@ -3,21 +3,21 @@ let ecosystem;
 
 function setup() {
   createCanvas(800, 600);
-  seed = getParameterByName("seed") || Math.round(random(1000));
+  seed = UtilFunctions.getParameterByName("seed") || Math.round(random(1000));
   randomSeed(seed);
   noiseSeed(seed);
 
   options = {
-    cellWidth: getParameterByName("cellWidth", "", parseInt)
-    , scale: getParameterByName("scale", "", Number)
-    , percentWater: getParameterByName("percentWater", "", Number)
-    , erosionRate: getParameterByName("erosionRate", "", Number)
+    cellWidth: UtilFunctions.getParameterByName("cellWidth", "", parseInt)
+    , scale: UtilFunctions.getParameterByName("scale", "", Number)
+    , percentWater: UtilFunctions.getParameterByName("percentWater", "", Number)
+    , erosionRate: UtilFunctions.getParameterByName("erosionRate", "", Number)
   };
-  unsetUndefineds(options);
+  UtilFunctions.unsetUndefineds(options);
 
   noiseOptions = {
-    octaves: getParameterByName("noise.octaves", "", parseInt) || 10
-    , falloff: getParameterByName("noise.falloff", "", Number) || 0.6
+    octaves: UtilFunctions.getParameterByName("noise.octaves", "", parseInt) || 10
+    , falloff: UtilFunctions.getParameterByName("noise.falloff", "", Number) || 0.6
   }
   noiseDetail(noiseOptions.octaves, noiseOptions.falloff);
 
@@ -40,23 +40,4 @@ function logSettings(){
   console.log(options);
   console.log("noise options: ");
   console.log(noiseOptions);
-}
-
-// via: https://stackoverflow.com/a/901144
-function getParameterByName(name, url, formatter) {
-  if (!url) url = window.location.href;
-  name = name.replace(/[\[\]]/g, "\\$&");
-  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-      results = regex.exec(url);
-  if (!results) return null;
-  if (!results[2]) return null;
-  value = decodeURIComponent(results[2].replace(/\+/g, " "));
-  if (formatter) return formatter(value);
-  return value;
-}
-
-// via: https://stackoverflow.com/a/38340730
-function unsetUndefineds(obj) {
-  Object.keys(obj).forEach((key) => (obj[key] == null) && delete obj[key]);
-  return obj;
 }
