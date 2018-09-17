fee8188639c657ba3b183d6cd916f9bd7a269b98
Brian Honohan <brian.honohan@gmail.com>
2018-09-16 23:07:52 -0400

    p5js/water-cycle - Refactor Terrain and Weather Grid into the 'system'
    Consolidate the internal data models of the Terrain and the WeatherGrid into a central 'system'.
    
    Intent here is to keep the app.js pretty simple, and just hook into mouse/keyboard events.

--- p5js/water-cycle/app.js
+++ p5js/water-cycle/app.js
@@ -1,7 +1,5 @@
 let seed;
 let system;
-let terrain;
-let weatherGrid;
 
 function setup() {
   createCanvas(windowWidth, windowHeight-35);
@@ -11,12 +9,9 @@ function setup() {
   frameRate(1);
   system.tick();
 
-  let rect = new Rect(0, 0, width/2, height/2);
-  terrain = new Terrain2D(rect);
-  weatherGrid = new CellGrid(rect, WeatherCell, 20);
 }
 
 function draw(){
   system.tick();
-  terrain.draw();
+  system.draw();
 }
--- p5js/water-cycle/classes/system.js
+++ p5js/water-cycle/classes/system.js
@@ -1,8 +1,18 @@
 class System {
-  constructor(){ 
+  constructor(){
+    let rect = new Rect(0, 0, width/2, height/2);
+    this.terrain = new Terrain2D(rect);
+    this.cellWidth = 2;
+
+    let weatherPos = new Rect(width/2, 0, width/2, height/2);
+    this.weatherGrid = new CellGrid(weatherPos, WeatherCell, this.cellWidth);
   }
 
   tick(){
     console.log("tock");
   }
-}
+
+  draw(){
+    this.terrain.draw();
+  }
+
