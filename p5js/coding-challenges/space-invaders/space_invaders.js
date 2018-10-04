53555d04a3b80de241f439433d2a02b20ead567a
Brian Honohan <brian.honohan@gmail.com>
2018-10-04 00:47:39 -0400

    p5js/space-invaders - Add wave of Invaders
    * Currently, the just move across the screen, bouncing off the max / min X range.
    * I'm trying to mimic the animation style where the bottom row moves over, then the row above, then the row above that.
    * BUG: they eventually get out of sync with each other ... there is something in off in the frame based animation.

--- p5js/coding-challenges/space-invaders/space_invaders.js
+++ p5js/coding-challenges/space-invaders/space_invaders.js
@@ -1,6 +1,7 @@
 var spaceship;
 var blockSize;
 var bullets;
+var invasionWave;
 
 function setup() {
   createCanvas(400, 400);
@@ -8,6 +9,9 @@ function setup() {
 
   spaceship = new Spaceship();
   bullets = [];
+  invasionWave = new InvasionWave(5,6);
+
+  // frameRate(10);
 }
 
 function draw() {
@@ -18,6 +22,9 @@ function draw() {
     bullets[i].draw();
     bullets[i].tick();
   }
+
+  invasionWave.tick();
+  invasionWave.draw();
 }
 
 function keyPressed() {
@@ -55,8 +62,6 @@ class Spaceship {
   }
 }
 
-
-
 class Bullet {
   constructor(x, y){
     this.x = x;
@@ -71,5 +76,152 @@ class Bullet {
 
   tick(){
     this.y -= 10;
+  }
+}
+
+class InvasionWave {
+  constructor(rows, cols){
+    this.rows = rows;
+    this.cols = cols;
+
+    this.direction = 1;
+    this.framesOfMovementPerRow = 10;
+    this.calcBounds();
+    this.initInvaders();
+  }
+
+  calcBounds(){
+    const range = width - InvasionWave.colSpacing; 
+    const numPotentialCols = floor(range / this.totalColumnWidth());
+    this.minAllowedX = InvasionWave.colSpacing + this.framesOfMovementPerRow;
+    this.maxAllowedX = this.minAllowedX + numPotentialCols * this.totalColumnWidth() - this.framesOfMovementPerRow;
+  }
+
+  totalColumnWidth(){
+    return Invader.width + InvasionWave.colSpacing;
+  }
+
+  static get colSpacing()   { return 5; }
+  static get rowSpacing()   { return 10; }
+
+  initInvaders(){
+    let baseX = this.minAllowedX + 3 * this.framesOfMovementPerRow;
+    // let baseX = this.maxAllowedX - this.cols * this.totalColumnWidth() - 0.5 * Invader.width - 3 * this.framesOfMovementPerRow;
+    // let baseX =  width - InvasionWave.colSpacing - 5 * (InvasionWave.colSpacing + Invader.width);
+    let baseY = 40;
+    let numPerRow = 6;
+
+    let tmpX;
+    let tmpY;
+    this.invaders = []
+
+    for (var i = 0; i < this.rows; i++) {
+      this.invaders[i] = [];
+      tmpY = baseY + i * (Invader.height + InvasionWave.rowSpacing);
+
+      for (var j = 0; j < this.cols; j++) {
+        tmpX = baseX + j * (Invader.width + InvasionWave.colSpacing);
+        this.invaders[i][j] = new Invader(tmpX, tmpY);
+      }
+    }
+  }
+
+  draw(){  
+    this.debugMaxMinX();
+
+    for (var i = 0; i < this.rows; i++) {
+      for (var j = 0; j < this.cols; j++) {
+        this.invaders[i][j].draw();
+      }
+    }
+  }
+
+  debugMaxMinX(){
+    stroke(255, 255, 0);
+    line(this.minAllowedX, 0, this.minAllowedX, height);
+    line(this.maxAllowedX, 0, this.maxAllowedX, height);
+    noStroke();
+  }
+
+  tick(){
+    let bottomRow = this.bottomRowStillInvading();
+
+    if (bottomRow == -1) {
+      console.log("You won!!");
+    }
+
+    const rowMoving = this.rows - 1 - (floor(frameCount / this.framesOfMovementPerRow) % this.rows);
+
+    if (bottomRow == rowMoving){
+      const maxMinX = this.getMaxMinX();
+      if (this.direction == 1 && maxMinX[1] > this.maxAllowedX){
+        this.direction = -1;
+      } else if (this.direction == -1 && maxMinX[0] <= this.minAllowedX){
+        this.direction = 1;
+      }
+    }
+
+    for (var j = 0; j < this.cols; j++) {
+      this.invaders[rowMoving][j].move(this.direction);
+    }
+  }
+
+  bottomRowStillInvading(){
+    let tmpBottomRow = -1;
+    let tmpInvader;
+    for (var i = 0; i < this.rows; i++) {
+      for (var j = 0; j < this.cols; j++) {
+        tmpInvader = this.invaders[i][j];
+        if (this.invaders[i][j].state == Invader.STATE_INVADING) {
+          tmpBottomRow = i;
+          break;
+        }
+      }
+    }
+    return tmpBottomRow;
+  }
+
+  getMaxMinX(){
+    let constraints = [width, 0]; // (min, max)
+    let tmpInvader;
+    for (var i = 0; i < this.rows; i++) {
+      for (var j = 0; j < this.cols; j++) {
+        tmpInvader = this.invaders[i][j];
+        if (tmpInvader.state == Invader.STATE_BLOWN_UP) {
+          continue;
+        }
+        constraints[0] = min(constraints[0], tmpInvader.x);
+        constraints[1] = max(constraints[1], tmpInvader.x + Invader.width);
+      }
+    }
+    return constraints;
+  }
+}
+
+class Invader {
+  constructor(x, y){
+    this.x = x;
+    this.y = y;
+    this.state = Invader.STATE_INVADING;
+  }
+
+  static get width()    { return 30; }
+  static get height()   { return 30; }
+
+  static get STATE_INVADING()   { return 0; }
+  static get STATE_BLOWN_UP()   { return 1; }
+
+  draw(){
+    if (this.state == Invader.STATE_BLOWN_UP){
+      return;
+    }
+    rectMode(CORNER);
+    noStroke();
+    fill(200,30,200);
+    rect(this.x, this.y, Invader.width, Invader.height);
+  }
+
+  move(direction){
+    this.x += 1 * direction;
   }
 }
