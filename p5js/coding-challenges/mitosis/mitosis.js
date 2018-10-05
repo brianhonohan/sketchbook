8c6813ac69eb173c9d17d436caf77fdff71f2b42
Brian Honohan <brian.honohan@gmail.com>
2018-10-05 19:30:43 -0400

    p5js/mitosis - Add a cell, and draw using simple ellipse
    * Establish some a basic concept of a cell, its membrane (drawn using the strokeWeight)

--- p5js/coding-challenges/mitosis/mitosis.js
+++ p5js/coding-challenges/mitosis/mitosis.js
@@ -1,7 +1,31 @@
+var cell;
+
 function setup(){
   createCanvas(windowWidth, windowHeight-45);
+
+  cell = new Cell(width/2, height/2);
 }
 
 function draw(){
   background(50);
+
+  cell.draw();
+}
+
+
+class Cell {
+  constructor(x, y) {
+    this.pos = createVector(x, y);
+    this.size = 150;
+  }
+
+  get x(){ return this.pos.x; }
+  get y(){ return this.pos.y; }
+
+  draw(){
+    fill(160, 180, 250);
+    stroke(40,100,120);
+    strokeWeight(5);
+    ellipse(this.x, this.y, this.size, this.size)
+  }
 }
