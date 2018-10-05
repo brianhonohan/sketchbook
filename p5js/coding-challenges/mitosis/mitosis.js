fc0e6c469dd5a80ed2487b4d34418486ea9da0be
Brian Honohan <brian.honohan@gmail.com>
2018-10-05 19:48:11 -0400

    p5js/mitosis - Add CellMembrane
    * Each segment is really just the starting point, and the membrane is an array of them.  (perhaps these could just be P5Vectors ...)

--- p5js/coding-challenges/mitosis/mitosis.js
+++ p5js/coding-challenges/mitosis/mitosis.js
@@ -17,15 +17,44 @@ class Cell {
   constructor(x, y) {
     this.pos = createVector(x, y);
     this.size = 150;
+    this.initMembrane();
   }
 
   get x(){ return this.pos.x; }
   get y(){ return this.pos.y; }
 
+  initMembrane(){
+    this.membrane = [];
+    const numSegments = 30;
+    let thetaStep = TWO_PI / numSegments;
+    let tmpSegment;
+    let tmpPoint = createVector(0, 0);
+
+    for (var i = 0; i < numSegments; i++){
+      tmpPoint.x = this.x + cos(thetaStep*i) * this.size/2;
+      tmpPoint.y = this.y + sin(thetaStep*i) * this.size/2; 
+      tmpSegment = new CellMembraneSegment(tmpPoint.x, tmpPoint.y);
+      this.membrane.push(tmpSegment);
+    }
+  }
+
   draw(){
     fill(160, 180, 250);
     stroke(40,100,120);
     strokeWeight(5);
-    ellipse(this.x, this.y, this.size, this.size)
+    beginShape();
+    for (var i = 0; i < this.membrane.length; i++){
+      vertex(this.membrane[i].x, this.membrane[i].y);
+    }
+    endShape(CLOSE);
   }
+}
+
+class CellMembraneSegment {
+  constructor(x, y) {
+    this.pos = createVector(x, y);
+  }
+
+  get x(){ return this.pos.x; }
+  get y(){ return this.pos.y; }
 }
