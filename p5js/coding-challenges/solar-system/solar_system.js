743423a9b74d477efb0ff0457ad68d4212850dac
Brian Honohan <brian.honohan@gmail.com>
2018-10-06 23:24:07 -0400

    p5js/solar-system - Basic system working
    * Determine inital oribtal velocities of the masses around the center start.
    * On each tick(), recalculate the force on each object (other than the star for now) and set its acceleration.
    
    TODO:
    -  Need to adjust the time scale, and allow for zoom in/out
    
    But there is orbital behavior

--- p5js/coding-challenges/solar-system/solar_system.js
+++ p5js/coding-challenges/solar-system/solar_system.js
@@ -9,8 +9,32 @@ function setup(){
 function draw(){
   background(25);
   system.draw();
+  system.tick();
 }
 
+class Physics {
+
+  // G: 6.67408 × 10-11 m3 kg-1 s-2
+  static get G_CONTSTANT(){ return 6.67408e-11; }
+
+  // points from Obj1 => Obj2
+  static forceDueToG(obj1, obj2){
+    let r = p5.Vector.sub(obj2.pos, obj1.pos);
+    let rSquared = r.magSq();
+    let forceMag = Physics.G_CONTSTANT * obj1.mass * obj2.mass / rSquared;
+    let force = r.setMag(forceMag);
+    return force;
+  }
+
+  static orbitalV(ofObject, aroundObj){
+    let r = p5.Vector.sub(aroundObj.pos, ofObject.pos);
+    let speed = pow(Physics.G_CONTSTANT * aroundObj.mass / r.mag() , 0.5);
+    let v = r.copy();
+    v.rotate(HALF_PI);
+    v.setMag(speed);
+    return v;
+  }
+}
 
 class SolarSystem {
   constructor() {
@@ -19,6 +43,7 @@ class SolarSystem {
   }
 
   static get scale_space(){ return 4.5e9 / width; }
+  static get scale_time(){ return 1; }
 
   init(){
     let numObjects = 10;
@@ -30,6 +55,7 @@ class SolarSystem {
         this.objects.push(this.generatePlanet());
       }
     }
+    this.setInitialOrbitalVelocities();
   }
 
   generateStar(idx){
@@ -44,11 +70,34 @@ class SolarSystem {
     randLoc.rotate(randTheta);
 
     // jupiter: 1.8982×1027 or 18,982x1023
-    // mecury   3.3011×1023
+    // mercury   3.3011×1023
     let mass = random(3, 10000) * 1e23;
 
     let surfaceColor = color(random(50, 200),random(50, 200),random(50, 200));
     return new MassiveObject(randLoc.x, randLoc.y, mass, surfaceColor);
+  }
+
+  setInitialOrbitalVelocities(){
+    let star = this.objects[0];
+    for(var i=1; i < this.objects.length; i++){
+      let obj = this.objects[i];
+      obj.velocity = Physics.orbitalV(obj, star);
+    }
+  }
+
+  tick(){
+    // for now, for simplicity
+    //  ... keep the star fixed (don't apply any forces to it)
+    let star = this.objects[0];
+    for(var i=1; i < this.objects.length; i++){
+      let obj = this.objects[i];
+      let force = Physics.forceDueToG(obj, star);
+      obj.accel = force.copy().div(obj.mass);
+    }
+
+    for(var i=1; i < this.objects.length; i++){
+      this.objects[i].tick();
+    }
   }
 
   draw(){
@@ -77,8 +126,8 @@ class MassiveObject {
   get y(){ return this.pos.y; }
 
   tick(){
-    this.pos.add(this.speed);
-    this.velocity.add(this.accel);
+    this.pos.add(this.velocity.mult(SolarSystem.scale_time));
+    this.velocity.add(this.accel.mult(SolarSystem.scale_time));
   }
 
   draw(){
