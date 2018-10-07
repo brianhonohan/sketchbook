678e85912e4d252fb41e3a8cb7e71f170d4cd50e
Brian Honohan <brian.honohan@gmail.com>
2018-10-06 22:31:53 -0400

    p5js/solar-system - Add basic system with objects
    * Using real numbers, which are not scaled yet, so the only object visible is the start at the center.

--- p5js/coding-challenges/solar-system/solar_system.js
+++ p5js/coding-challenges/solar-system/solar_system.js
@@ -1,7 +1,78 @@
+var system;
+
 function setup(){
   createCanvas(windowWidth, windowHeight);
+
+  system = new SolarSystem();
 }
 
 function draw(){
   background(25);
+  system.draw();
+}
+
+
+class SolarSystem {
+  constructor() {
+    this.objects = [];
+    this.init();
+  }
+
+  init(){
+    let numObjects = 10;
+    for(var i=0; i<numObjects; i++){
+
+      if(i == 0){
+        this.objects.push(this.generateStar());
+      }else{
+        this.objects.push(this.generatePlanet());
+      }
+    }
+  }
+
+  generateStar(idx){
+    return new MassiveObject(0, 0, 1.9885e30);
+  }
+
+  generatePlanet(idx){
+    // our solar system is 4.5e9 km along major axis;
+    // mecury, perihelion: 46,001,200 km
+    let randX = random(40, 2500) * 1e6; // km
+    let randY = random(40, 2500) * 1e6;
+
+    // jupiter: 1.8982×1027 or 18,982x1023
+    // mecury   3.3011×1023
+    let mass = random(3, 20000) * 1e23;
+    return new MassiveObject(randX, randY, mass);
+  }
+
+  draw(){
+    push();
+    translate(width/2, height/2);
+    for(var i=0; i < this.objects.length; i++){
+      this.objects[i].draw();
+    }
+    pop();
+  }
+}
+
+class MassiveObject {
+  constructor(x, y, mass) {
+    this.pos = createVector(x, y);
+    this.accel = createVector(0, 0);
+    this.speed = createVector(0, 0);
+    this.mass = mass; // kg
+  }
+
+  get x(){ return this.pos.x; }
+  get y(){ return this.pos.y; }
+
+  tick(){
+    this.pos.add(this.speed);
+    this.speed.add(this.accel);
+  }
+
+  draw(){
+    ellipse(this.x, this.y, 10, 10);
+  }
 }
