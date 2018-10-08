748e4bf5369e65120cb6bbeb1f06f8d7fa621b78
Brian Honohan <brian.honohan@gmail.com>
2018-10-07 22:21:16 -0400

    p5js/maze-generator - Establish basic grid
    * Using existing CellGrid utility class for managing grids

--- p5js/coding-challenges/maze-generator/maze_generator.js
+++ p5js/coding-challenges/maze-generator/maze_generator.js
@@ -1,7 +1,48 @@
+var maze;
+
 function setup(){
   createCanvas(windowWidth, windowHeight);
+  maze = new Maze(width, height);
+  frameRate(1);
 }
 
 function draw(){
   background(0);
+
+  maze.draw();
+}
+
+class Maze {
+  constructor(_width, _height) {
+    this.cellWidth = 200;
+    let mazeWidth = floor(width / this.cellWidth) * this.cellWidth;
+    let mazeHeight = floor(height / this.cellWidth) * this.cellWidth;
+    this.mazeSize = new Rect(0, 0, mazeWidth, mazeHeight);
+    this.grid = new CellGrid(this.mazeSize, this, this.cellWidth);
+  }
+
+  createCell(tmpRow, tmpCol, i){
+    return new MazeCell(tmpRow, tmpCol, i);
+  }
+
+  draw(){
+    this.grid.renderViews();
+  }
+}
+
+class MazeCell {
+  constructor(row, col, index){
+    // variable names are dictated by CellGrid
+    this._row = row;
+    this._col = col;
+    this._idx = index;
+  }
+}
+
+// Class name is dictated by CellGrid
+class CellViewer {
+  renderCell(tmpCell, tmpX, tmpY, cellWidth, cellHeight){
+    fill(50);
+    rect(tmpX, tmpY, cellWidth, cellHeight);
+  }
 }
