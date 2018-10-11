3fd4eec3b63130f236298397631a223aed04f754
Brian Honohan <brian.honohan@gmail.com>
2018-10-11 19:55:24 -0400

    p5js/terrain-3d - Add basic Perlin noise driven elevation
    * Establishes basic display of terrain using the noise(x, y) function.

--- p5js/coding-challenges/terrain-3d/terrain_3d.js
+++ p5js/coding-challenges/terrain-3d/terrain_3d.js
@@ -1,7 +1,35 @@
+var terrain;
+
 function setup(){
   createCanvas(windowWidth, windowHeight);
+
+  terrain = new Terrain();
 }
 
 function draw(){
   background(45);
+
+  let baseColor = color(30, 90, 30);
+  let maxColor = color(230, 240, 230);
+
+  let cellSize = 10;
+  noStroke();
+  for (var x = 0; x < width; x += cellSize){
+    for (var z = 0; z < height; z += cellSize){
+      let heightAtLoc = terrain.elevationAt(x, z);
+      fill( lerpColor(baseColor, maxColor, heightAtLoc) );
+      rect(x, z, cellSize, cellSize);
+    }
+  }
+}
+
+
+class Terrain {
+  constructor(){
+    this.noiseScale = 0.005;
+  }
+
+  elevationAt(x, z){
+    return noise(x * this.noiseScale, z * this.noiseScale);
+  }
 }
