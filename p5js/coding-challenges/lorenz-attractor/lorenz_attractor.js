a1b138ec13af9595e23bd9133b86e2c206cc6f31
Brian Honohan <brian.honohan@gmail.com>
2018-10-14 17:33:30 -0400

    p5js/lorenz-attractor - Adds Basic rendering
    (Not sure of this approach)
    * Adds a 'LorenzSystemViewer' which renders the system for a given starting point, a certain number of iterations ...
    * Perhaps simple object with X / Y / Z would be better than p5 Vector and subsequent copying that is necessary.
    
    Some fine tuning
    - 'timeStep'  ... started this at 1 ... but was seeing NaN values for x, y, z
    - Returning the Nth point (currently the 10th point) generated ... this allowed for some basic animation.

--- p5js/coding-challenges/lorenz-attractor/lorenz_attractor.js
+++ p5js/coding-challenges/lorenz-attractor/lorenz_attractor.js
@@ -1,20 +1,25 @@
 var lorenzSystem;
 var cam;
 var cameraController;
+var lsViewer;
+var currentPoint;
 
 function setup(){
   createCanvas(windowWidth, windowHeight, WEBGL);
 
   lorenzSystem = new LorenzSystem(28, 10, 8/3.0);
+  lsViewer = new LorenzSystemViewer();
 
   cam = createCamera();
   cameraController = new CameraController(cam);
-  debugMode();
+  currentPoint = createVector(1, 1, 1);
 }
 
 function draw(){
   background(45);
   cameraController.tick();
+  scale(6);
+  currentPoint = lsViewer.renderStartingAt(lorenzSystem, currentPoint, 600);
 }
 
 class LorenzSystem {
@@ -34,5 +39,38 @@ class LorenzSystem {
 
   dz_dt (loc){
     return loc.x * loc.y - this.beta * loc.z;
+  }
+
+  nextLocation(loc, timeStep){
+    let newPoint = loc.copy();
+    newPoint.x += this.dx_dt(loc) * timeStep;
+    newPoint.y += this.dy_dt(loc) * timeStep;
+    newPoint.z += this.dz_dt(loc) * timeStep;
+    return newPoint;
+  }
+}
+
+class LorenzSystemViewer {
+  constructor(){
+    this.timeStep = 0.005;
+  }
+
+  renderStartingAt(system, loc, numIterations){
+    let nextPoint;
+    let pointToReturn;
+    for (var i = 0; i < numIterations; i++){
+      nextPoint = system.nextLocation(loc, this.timeStep);
+
+      if (i == 10) {
+        pointToReturn = nextPoint;
+      }
+      stroke(85+ i%255, 170+ i%255, 255 + i%255);
+      line(loc.x, loc.y, loc.z, nextPoint.x, nextPoint.y, nextPoint.z);
+
+      // console.log(`x: ${loc.x}, y: ${loc.y}, z: ${loc.z}`)
+      loc = nextPoint;
+    }
+
+    return pointToReturn;
   }
 }
