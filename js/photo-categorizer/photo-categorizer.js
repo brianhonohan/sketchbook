cd23b688f933977d7385d5cd0c2676d50322f74e
Brian Honohan <brian.honohan@gmail.com>
2018-10-13 22:20:06 -0400

    js/photo-categorizer - Add basic categorization on swipe
    * Removes the swiped item, and the dupe of the newly shown item.
        - this gets a bit tricky when swiping left because the array is being modified to the left of the 'activeIndex' which is shifting down accordingly.
    * Update PhotoCategorizer to now maintain an object array for the slides its showing, and what category each item has been applied to.
    
    Note: In a browser, if I end the swip gesture outside of the viewable area, the callback events don't fire.

--- js/photo-categorizer/photo-categorizer.js
+++ js/photo-categorizer/photo-categorizer.js
@@ -1,7 +1,11 @@
 class PhotoCategorizer {
   constructor(){
     this.slideRenderBuffer = 2; // Render the next 2 slides
+    this.currentIdx = 0;
   }
+
+  static get DIRECTION_LEFT() { return -1; }
+  static get DIRECTION_RIGHT() { return 1; }
 
   initSwiper(){
     this.swiper = new Swiper ('.swiper-container', this.swiperConfig());
@@ -10,6 +14,7 @@ class PhotoCategorizer {
   swiperConfig(){
     return {
       initialSlide: this.slideRenderBuffer,
+      runCallbacksOnInit: false,
       on: {
           // slideChangeTransitionStart: this.handleSlideChangeTransitionStart,
           // slideChangeTransitionEnd: this.handleSlideChangeTransitionEnd,
@@ -34,11 +39,17 @@ class PhotoCategorizer {
   handleSlideChangeTransitionEnd(){ categorizer.log("slideChangeTransitionEnd"); }
 
   // Same as 'slideChangeTransition...', but only when dragging to left 
-  handleSlideNextTransitionStart(){ categorizer.log("slideNextTransitionStart"); }
+  handleSlideNextTransitionStart(){
+    categorizer.log("slideNextTransitionStart");
+    categorizer.handleSwipe(PhotoCategorizer.DIRECTION_LEFT);
+  }
   handleSlideNextTransitionEnd(){ categorizer.log("slideNextTransitionEnd"); }
 
   // Same as 'slideChangeTransition...', but only when dragging to right 
-  handleSlidePrevTransitionStart(){ categorizer.log("slidePrevTransitionStart"); }
+  handleSlidePrevTransitionStart(){ 
+    categorizer.log("slidePrevTransitionStart"); 
+    categorizer.handleSwipe(PhotoCategorizer.DIRECTION_RIGHT);
+  }
   handleSlidePrevTransitionEnd(){ categorizer.log("slidePrevTransitionEnd"); }
 
   // Fired as soon as user dragging starts/stops, regardless if slide transitions
@@ -51,6 +62,7 @@ class PhotoCategorizer {
 
   loadSlides(){
     this.slides = ['Slide 1', 'Slide 2', 'Slide 3', 'Slide 4', 'Slide 5'];
+    this.slides = this.slides.map((sl) => { return {data: sl, category: null}; });
   }
 
   initialSlides(){
@@ -65,9 +77,67 @@ class PhotoCategorizer {
     return slideSet;
   }
 
+  handleSwipe(direction){
+    this.categorize(this.currentIdx, this.getCategoryFor(direction));
+    if (direction === PhotoCategorizer.DIRECTION_LEFT){
+      this.handleSwipeLeft();
+    }else if (direction === PhotoCategorizer.DIRECTION_RIGHT){
+      this.handleSwipeRight();
+    }
+    if (this.currentIdx < this.slides.length - 1){
+      this.currentIdx++;
+    }
+  }
+
+  handleSwipeLeft(){
+    // Slightly tricky, because we're removing items to the left
+    // the activeIndex will be altered ...
+
+    // remove the item that was classified
+    let idxToRemove = this.swiper.previousIndex;
+    this.swiper.removeSlide(idxToRemove);
+
+    // activeIndex will now be previousIndex, so go one further to the left
+    idxToRemove = Math.max(0, idxToRemove - 1);
+    if (idxToRemove > 0) {
+      // Don't remove the 0 index slide,
+      // Swiper doesn't handle this well with only 2 items left 
+      // More likley I don't understand what its doing
+      this.swiper.removeSlide(idxToRemove);
+    }
+
+    this.swiper.update();
+  }
+
+  handleSwipeRight(){
+    // Simple, because we're removing items to the right
+    // the activeIndex won't be altered ...
+
+    // remove the item that was classified
+    this.swiper.removeSlide(this.swiper.previousIndex);
+
+    // remove the dupe of the current side
+    this.swiper.removeSlide(this.swiper.previousIndex);
+
+    this.swiper.update();
+  }
+
+  getCategoryFor(direction){
+    if (direction === PhotoCategorizer.DIRECTION_LEFT){
+      return 'Left';
+    }else if (direction === PhotoCategorizer.DIRECTION_RIGHT){
+      return 'Right';
+    }
+  }
+
+  categorize(idx, category){
+    console.log(`Categorizing index: ${idx} as category: ${category}`);
+    this.slides[idx].category = category;
+  }
+
   handleRenderSlide(slide, index){
     console.log("rendering: " + index);
-    return `<div class="swiper-slide">${slide}</div>`;
+    return `<div class="swiper-slide">${slide.data}</div>`;
   }
 }
 
