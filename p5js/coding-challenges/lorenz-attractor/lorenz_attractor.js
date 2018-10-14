3edd357fbd182a2f196da03dcedc8dd12a025057
Brian Honohan <brian.honohan@gmail.com>
2018-10-14 16:48:48 -0400

    p5js/lorenz-attractor - Establish basic system questions
    * Lifted from:
      https://en.wikipedia.org/wiki/Lorenz_system#Overview
    
    And using the recommended default values.

--- p5js/coding-challenges/lorenz-attractor/lorenz_attractor.js
+++ p5js/coding-challenges/lorenz-attractor/lorenz_attractor.js
@@ -1,7 +1,29 @@
+var lorenzSystem;
+
 function setup(){
   createCanvas(windowWidth, windowHeight, WEBGL);
+
+  lorenzSystem = new LorenzSystem(28, 10, 8/3.0);
 }
 
 function draw(){
   background(45);
+}
+  constructor(rho, sigma, beta){
+    this.rho = rho;
+    this.sigma = sigma;
+    this.beta = beta;
+  }
+
+  dx_dt (loc){
+    return this.sigma * (loc.y - loc.x);
+  }
+
+  dy_dt (loc){
+    return loc.x * (this.rho - loc.z) - loc.y;
+  }
+
+  dz_dt (loc){
+    return loc.x * loc.y - this.beta * loc.z;
+  }
 }
