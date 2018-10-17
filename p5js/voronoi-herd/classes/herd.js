53c808eec4ef83226bfad9e3da2716583a0cfeeb
Brian Honohan <brian.honohan@gmail.com>
2018-10-16 23:54:05 -0400

    p5js/voronoi-herd - Update initial herd spacing to use Gaussion distribution
    * Use randomGaussian() to place new HerdMembers (so they are mostly in a cluster to start).
      - https://p5js.org/reference/#/p5/randomGaussian
    
    Sidenote: Just learned Gauss was a contemporary of: Sophie Germain, https://en.wikipedia.org/wiki/Sophie_Germain ... a 19th Century mathemetician, who contributed to our understanding of acoustics, elasticity, and Number theory.

--- p5js/voronoi-herd/classes/herd.js
+++ p5js/voronoi-herd/classes/herd.js
@@ -1,5 +1,7 @@
 class Herd {
   constructor(params){
+    this.startingX = random(width);
+    this.startingY = random(height);
     this.params = params;
     this.herdCount = this.params.herd_count;
     this.initHerd();
@@ -7,7 +9,13 @@ class Herd {
 
   initHerd(){
     this.members = Array(this.herdCount).fill()
-                    .map((_, i) => new HerdMember(random(width), random(height)) );
+                    .map((_, i) => this.spawnRandomMember() );
+  }
+
+  spawnRandomMember(){
+    let approxSpacing = map(this.herdCount, 0, 100, 30, 300);
+    return new HerdMember( constrain(randomGaussian(this.startingX, approxSpacing), 0, width),
+                           constrain(randomGaussian(this.startingY, approxSpacing), 0, height));
   }
 
   draw(){
