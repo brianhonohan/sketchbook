6cfac262c5ae5c41d6edf92e00fe8d60d53b4fd1
Brian Honohan <brian.honohan@gmail.com>
2018-10-22 21:27:19 -0400

    p5js/fractal-trees - Add simple recursive FractalTree
    * Only draws it once
    * Angles are not quite right, but illustrate the general approach
    * Utilizes the tranform matrix to move the "cursor" / "pen" to the end of the line just drawn, so that it is a reference point for other lines to be drawn on the same branch.

--- p5js/coding-challenges/fractal-trees-01/app.js
+++ p5js/coding-challenges/fractal-trees-01/app.js
@@ -1,7 +1,19 @@
+var tree;
+
 function setup(){
   createCanvas(windowWidth, windowHeight);
+
+  tree = new FractalTree();
+  stroke(200);
+  drawTree();
 }
 
-function draw(){
+function drawTree(){
   background(50);
+
+  push();
+  translate(tree.x, tree.y);
+  rotate(PI);
+  tree.draw(0);
+  pop();
 }
