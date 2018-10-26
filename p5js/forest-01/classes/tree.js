cc0b0513d5e964e69d4108baa643e5426f2b019f
Brian Honohan <brian.honohan@gmail.com>
2018-10-26 15:39:15 -0400

    p5js/forest - Add concept of shadow radius to Trees
    * As Trees grow older, they cast a wider shadow on the ground beneath them.

--- p5js/forest-01/classes/tree.js
+++ p5js/forest-01/classes/tree.js
@@ -6,6 +6,7 @@ class Tree {
   }
 
   static get MAX_AGE() { return 2000; }
+  static get MAX_SHADOW_RADIUS() { return 100; }
 
   tick(){
     this.age += 1;
@@ -16,5 +17,21 @@ class Tree {
     fill(50, 120, 50);
     rectMode(CENTER);
     rect(this.x, this.y, 10, 10);
+
+    let shadowWidth = this.shadowRadius() * 2;
+    fill(50, 50, 50, 50);
+    ellipse(this.x, this.y, shadowWidth, shadowWidth);
+  }
+
+  shadowRadius(){
+    let shadowFactor = 0;
+    if (this.age < 400) {
+      shadowFactor = map(this.age, 0, 400, 0, 0.2);
+    } else if (this.age < 1500) {
+      shadowFactor = map(this.age, 400, 1500, 0.2, 1);
+    } else {
+      shadowFactor = map(this.age, 1500, Tree.MAX_AGE, 1, 0.8);
+    }
+    return shadowFactor * Tree.MAX_SHADOW_RADIUS;
   }
 }
