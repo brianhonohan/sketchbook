c0e2f4173ba788f31a8505041e8d61ab1b517900
Brian Honohan <brian.honohan@gmail.com>
2018-11-08 22:48:59 -0500

    p5js/fractal-tree-oop - Add concept of SeasonalTime
    * Want to add seasonal behavior to the tree growh (budding of leaves in spring, slower/minimal growth in winter).

--- p5js/coding-challenges/fractal-trees-03/app.js
+++ p5js/coding-challenges/fractal-trees-03/app.js
@@ -1,15 +1,18 @@
 var tree;
 var groundLevel;
+var seasonalTime;
 
 function setup(){
   createCanvas(windowWidth, windowHeight);
 
   groundLevel = floor(height * 0.6);
   tree = new Tree( floor(width/2), groundLevel);
+  seasonalTime = new SeasonalTime(0.01);
 }  
 
 function draw(){
   background(50);
+  seasonalTime.tick();
   tree.tick();
   tree.draw();
   drawGround(groundLevel);
