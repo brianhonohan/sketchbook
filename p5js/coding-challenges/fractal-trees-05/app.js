ed114c9830a375ea9b04305fd55e1015c1d30676
Brian Honohan <brian.honohan@gmail.com>
2018-11-17 19:18:28 -0500

    p5js/space-colonization - Establish ground level
    * I plan on establish the root systems of plants competing over soil resources

--- p5js/coding-challenges/fractal-trees-05/app.js
+++ p5js/coding-challenges/fractal-trees-05/app.js
@@ -1,8 +1,18 @@
+var groundLevel;
+
 function setup() {
   createCanvas(windowWidth, windowHeight-35);
   P5JsSettings.init();
+
+  groundLevel = floor(height * 0.1);
 }
 
 function draw(){
   background(50);
+  drawGround(groundLevel);
+}
+
+function drawGround(y){
+  stroke(230);
+  P5JsUtils.drawSolidBoundary(0.1 * width, y, 0.9 * width, y);
 }
