86dac76feaa594b2841fb7fc35020d9f1ba1316a
Brian Honohan <brian.honohan@gmail.com>
2018-11-18 20:09:55 -0500

    p5js/space-colonization - Update RootSegment to detect nutrients
    * The RootSegments now have a detection range (visualized by a square)
        - Planning on using simple rectangle detection area to narrow the list of objects to perform a "distance" check on (which involves a lot more floating-point operations).

--- p5js/coding-challenges/fractal-trees-05/classes/root_segment.js
+++ p5js/coding-challenges/fractal-trees-05/classes/root_segment.js
@@ -2,16 +2,26 @@ class RootSegment {
   constructor(x, y, parent){
     this.pos = createVector(x, y);
     this.parent = parent;
+    this.nutrientDectionRange = 50;
   }
 
   get x(){ return this.pos.x; }
   get y(){ return this.pos.y; }
+
+  detectionArea(){
+    return new Rect(this.x - this.nutrientDectionRange / 2, 
+                    this.y - this.nutrientDectionRange / 2,
+                    this.nutrientDectionRange,
+                    this.nutrientDectionRange);
+  }
 
   static setStyle(){
     stroke(230);
   }
 
   draw(){
+    fill(50,200,50,80);
+    P5JsUtils.drawRect(this.detectionArea());
     line(this.parent.x, this.parent.y, this.x, this.y);
   }
 }
