bae0af117710cca9978b55401b6c896190643783
Brian Honohan <brian.honohan@gmail.com>
2018-11-27 23:31:25 -0500

    p5js/meandering-river - Refactor RiverSource data storage of heading
    * This uses a Javascript setter to ensure that the new `directionalVector` is recalculated whenever the heading is changed.
        - and uses this to draw the vector as an arrow.
    
    Reference:
    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/set

--- p5js/meandering-river/classes/river_source.js
+++ p5js/meandering-river/classes/river_source.js
@@ -1,8 +1,14 @@
 class RiverSource {
   constructor(pos, heading){
     this.pos = pos;
+    this.directionalVector = null;
     this.heading = heading;
-    this.virtualTarget = p5.Vector.add(pos, createVector(20, 0).rotate(heading));
+  }
+
+  set heading(newHeading){
+    this._heading = newHeading;
+    this.directionalVector = createVector(20, 0).rotate(newHeading);
+  }
   }
 
   get x(){ return this.pos.x; }
@@ -17,6 +23,6 @@ class RiverSource {
     ellipse(this.x, this.y, 10, 10);
 
     stroke(200, 200, 50);
-    P5JsUtils.drawArrow(this.pos, this.virtualTarget);
+    P5JsUtils.drawArrow(this.pos, p5.Vector.add(this.pos, this.directionalVector) );
   }
 }
