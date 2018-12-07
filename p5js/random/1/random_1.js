c147092c96bd8bcb13a91d94e999599d61c2aad2
Brian Honohan <brian.honohan@gmail.com>
2018-12-06 23:40:29 -0500

    p5js/random-1 - Add click-and-drag to draw circles

--- p5js/random/1/random_1.js
+++ p5js/random/1/random_1.js
@@ -1,15 +1,50 @@
 var canvas;
+var currentShape;
+var shapes;
 
 function setup(){
   canvas = createCanvas(500, 500);
+  colorMode(HSB);
+
+  ellipseMode(CENTER);
+  shapes = [];
 }
 
 function draw(){
+  background(47, 10, 10, 100);
 
+  shapes.forEach(s => s.draw());
 }
 
 function keyTyped(){
   if (key == 'p'){
     saveCanvas(canvas, 'screenshot', 'png');
+  }
+}
+
+function mousePressed(){
+  currentShape = new Circle(mouseX, mouseY);
+  shapes.push(currentShape);
+}
+
+function mouseDragged(){
+  currentShape.size = dist(currentShape.x, currentShape.y, mouseX, mouseY);
+}
+
+function mouseRelease(){
+  currentShape = null;
+}
+
+class Circle {
+  constructor(x, y){
+    this.pos = createVector(x, y);
+    this.size = 1;
+  }
+
+  get x(){ return this.pos.x; }
+  get y(){ return this.pos.y; }
+
+  draw(){
+    ellipse(this.x, this.y, this.size*2, this.size*2);
   }
 }
