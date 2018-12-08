5392dcc5e38b7c3867fc032ee87f45b914946b9c
Brian Honohan <brian.honohan@gmail.com>
2018-12-08 15:44:35 -0500

    p5js/random-1 - Move DrawModeManager to separate file

--- p5js/random/1/random_1.js
+++ p5js/random/1/random_1.js
@@ -34,53 +34,6 @@ function keyTyped(){
   }
 }
 
-class DrawModeManager {
-  constructor(){
-    this.defaultFill = color(50, 100, 100, 100);
-    this.fill(this.defaultFill);
-
-    this.strokeColor = null;
-    this.defaultStroke = color(150, 30, 30, 100);
-    this.strokeEnabled = false;
-    this.stroke(this.defaultStroke)
-  }
-
-  toggleFill(){
-
-    if (this.fillMode) {
-      this.noFill();
-    } else {
-      this.fill(this.fillColor);
-    };
-  }
-
-  fill(colorVal){
-    this.fillMode = true;
-    this.fillColor = colorVal;
-    fill(colorVal);
-  }
-
-  noFill(){
-    this.fillMode = false;
-    noFill();
-  }
-
-  toggleStroke(){
-    this.strokeEnabled ? this.noStroke() : this.stroke(this.strokeColor);
-  }
-
-  stroke(colorVal){
-    this.strokeEnabled = true;
-    this.strokeColor = colorVal;
-    stroke(colorVal);
-  }
-
-  noStroke(){
-    this.strokeEnabled = false;
-    noStroke();
-  }
-}
-
 function mousePressed(){
   currentShape = new Circle(mouseX, mouseY);
   shapes.push(currentShape);
