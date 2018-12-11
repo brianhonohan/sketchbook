b9ae0c4729907ed6369cdd1dfbe406cd7c40e63d
Brian Honohan <brian.honohan@gmail.com>
2018-12-11 01:05:20 -0500

    p5js/random-2 - Draw shapes instead of individual points
    * Instead of drawing points, draw a shape using p5.js' beginShape / endShape
    * Also add 3 modes for creating vertices
      - vertex ... simple vertex() for each point (results in straight lines)
      - curveVertex ... results in an open-end shape.
      - bezierVertex ... sudo curves, need to figure out how close out this shape a bit better.

--- p5js/random/2/random_2.js
+++ p5js/random/2/random_2.js
@@ -1,26 +1,84 @@
 var canvas;
 var particles;
+var shapeMode;
 
 function setup(){
   canvas = createCanvas(500, 500);
   particles = [];
 
+  shapeMode = 'vertex';
   colorMode(HSB);
+  drawBackground();
+}
+
+function drawBackground(){
   background(20);
 }
 
 function draw(){
-  // background(20);
-
-  stroke(frameCount % 255, 200, 200, 100);
   particles.forEach(p => p.tick());
-  particles.forEach(p => p.draw());
+
+  stroke(frameCount % 360, 80, 30, 1);
+  fill(frameCount % 360, 80, 90, 0.2);
+
+  if (frameCount % 5 == 0){
+    switch(shapeMode){
+      case 'vertex':
+        drawLines();
+        break;
+      case 'curveVertex':
+        drawCurves();
+        break;
+      case 'bezierVertex':
+        drawBeziers();
+        break;
+    }
+  }
+}
+
+function drawLines(){
+  beginShape();
+  particles.forEach(p => vertex(p.x, p.y));
+  endShape(CLOSE);
+}
+
+function drawCurves(){
+  beginShape();
+  particles.forEach(p => curveVertex(p.x, p.y));
+  endShape(CLOSE);
+}
+
+function drawBeziers(){
+  beginShape();
+  for(var i = 0; i < particles.length; i += 4){
+    let particle = particles[i];
+
+    vertex(particle.x, particle.y);
+
+    let part1 = particles[(i + 1) % particles.length];
+    let part2 = particles[(i + 2) % particles.length];
+    let part3 = particles[(i + 3) % particles.length];
+    bezierVertex(part1.x, part1.y, part2.x, part2.y, part3.x, part3.y);
+  }
+  endShape(CLOSE);
 }
 
 function keyTyped(){
   switch(key) {
     case 'p':
       saveCanvas(canvas, 'screenshot', 'png');
+      break;
+    case 'c':
+      drawBackground();
+      break;
+    case 'l':
+      shapeMode = 'vertex';
+      break;
+    case 'v':
+      shapeMode = 'curveVertex';
+      break;
+    case 'b':
+      shapeMode = 'bezierVertex';
       break;
   }
 }
