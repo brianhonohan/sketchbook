5a9af29a732e36c0155c7e387c3eaed013d37dcf
Brian Honohan <brian.honohan@gmail.com>
2018-12-16 17:29:22 -0500

    p5js/pulleys - Initial commit

---
+++ p5js/pulleys/app.js
@@ -0,0 +1,17 @@
+var canvas;
+
+function setup() {
+  canvas = createCanvas(500, 500);
+}
+
+function draw(){
+  background(50);
+}
+
+function keyTyped(){
+  switch(key) {
+    case 'p':
+      saveCanvas(canvas, 'screenshot', 'png');
+      break;
+  }
+}
