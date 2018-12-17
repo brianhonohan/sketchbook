70ee1330e787a8f177a714f377d1746ec1ca512e
Brian Honohan <brian.honohan@gmail.com>
2018-12-16 23:39:35 -0500

    p5js/pulleys - Add simple concept of a Pulley
    * Adds a prelimary / rudimentary representation of a Pulley
    * Pulleys have a x,y location, and a radius, and as with other p5.js classes, can draw themselves.
    * Additionally, Pulley objects have the ability to determine the 'tangentPoint' (not sure of the name, given a point outside of
      - Intent of this
    
    This version of the sketch draws a "rope" from the mouse's location to a Pulley placed in the system. Pressing the mouse button will toggle if the rope will wrap around the pulley in a clockwise or counter-clockwise direction.
    
    Note:
    The `mouseLoc` object is necessary because of the transform matrix and that the system deals with local coordinates.
      - I intended to explore this a bit furtehr.
      - I'm curious about the management overhead of having multiple "view panes" of interactivity that deal with local coordinates and can be placed anywhere on the canvas.
      - (They should check if the mouse is even in their area before proceeding, but this is just a sketch ... so not too strict just yet).

---
+++ p5js/pulleys/classes/pulley.js
@@ -0,0 +1,26 @@
+class Pulley {
+  constructor(x, y, radius = 10){
+    this.pos = createVector(x, y);
+    this.radius = 10;
+  }
+
+  get x() { return this.pos.x; }
+  get y() { return this.pos.y; }
+
+  tangentPoint(point, clockwiseWrap = true){
+    const vec = createVector(this.x - point.x, this.y - point.y);
+    const dist = vec.mag();
+    const theta = Math.asin(this.radius / dist);
+
+    const rotationAngle = clockwiseWrap ? -1 * theta : theta;
+    vec.rotate(rotationAngle);
+    return {x: point.x + vec.x, y: point.y + vec.y};
+  }
+
+  draw(){
+    noFill();
+    strokeWeight(2);
+    ellipseMode(CENTER);
+    ellipse(this.x, this.y, this.radius * 2, this.radius * 2);
+  }
+}
--- p5js/pulleys/classes/system.js
+++ p5js/pulleys/classes/system.js
@@ -1,6 +1,7 @@
 class System {
   constructor(p_xSizeAndPos){
     this.area = p_xSizeAndPos;
+    this.pulley = new Pulley(this.area.centerX, 20);
   }
 
   get x() { return this.area.x; }
@@ -18,6 +19,16 @@ class System {
   draw(){
     push();
     translate(this.x, this.y);
+    this.pulley.draw();
+
+    // draw rope, tangent to the pulley
+    stroke(0, 255, 0);
+    strokeWeight(1);
+    let mouseLoc = {x: mouseX - 0.1 * width, y: mouseY - 0.1 * height};
+    let clockwiseWrap = mouseIsPressed;
+    let tangPt = this.pulley.tangentPoint(mouseLoc, clockwiseWrap);
+    line(mouseLoc.x, mouseLoc.y, tangPt.x, tangPt.y);
+
     pop();
   }
 }
