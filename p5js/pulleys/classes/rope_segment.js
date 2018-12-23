f39100f1a3dc324f80e01916d83f9a617fe5be91
Brian Honohan <brian.honohan@gmail.com>
2018-12-23 14:52:15 -0500

    p5js/pulleys - Refactor RopeSegment draw() for simplicity
    * Isolated the calculation of the 'nextPoint' (aka endPt) since the draw() method was doing too much.

--- p5js/pulleys/classes/rope_segment.js
+++ p5js/pulleys/classes/rope_segment.js
@@ -5,15 +5,16 @@ class RopeSegment {
     this.endObj   = end;
   }
 
-  draw(){
-    let mouseLoc = {x: system.mouseX, y: system.mouseY};
-    let nextPoint;
-
+  endPt(){
     if (this.endObj) {
-      nextPoint = this.endObj.ropeAttachmentPoint(this.startObj);
-    } else {
-      nextPoint = mouseLoc;
+      return this.endObj.ropeAttachmentPoint(this.startObj);
+    }else{
+      return {x: system.mouseX, y: system.mouseY};
     }
+  }
+
+  draw(){
+    let nextPoint = this.endPt();
     let startTieOff = this.startObj.ropeAttachmentPoint(nextPoint);
 
     line(startTieOff.x, startTieOff.y, nextPoint.x, nextPoint.y);
