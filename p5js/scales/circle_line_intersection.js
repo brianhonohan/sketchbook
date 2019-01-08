0e11533b12ea5d456fb050fb345e1dc929dadf60
Brian Honohan <brian.honohan@gmail.com>
2019-01-07 22:50:14 -0500

    p5js/scales - Circle-Line Intersection - Add basic Circle and LineSegment

--- p5js/scales/circle_line_intersection.js
+++ p5js/scales/circle_line_intersection.js
@@ -1,11 +1,23 @@
 var canvas;
+var circle;
+var lineSegment;
 
 function setup(){
   canvas = createCanvas(500, 500);
+
+  circle = new Circle(300, height/2,  100);
+  lineSeg = new LineSegment(150, 350, 400, 100);
 }
 
 function draw(){
   background(50);
+
+  noFill();
+  stroke(230);
+  strokeWeight(2);
+  circle.draw();
+
+  lineSeg.draw();
 }
 
 function keyTyped(){
