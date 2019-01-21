877a338fbcd6b0365dbefd6814ed9b4ec7b4f205
Brian Honohan <brian.honohan@gmail.com>
2019-01-21 17:18:14 -0500

    p5js/keyviz - Refactor Control Panel to use button config array
    * Allows for easier iteration over buttons' config, calculate the yPos of the buttons

--- p5js/keyviz/classes/keyviz_control_panel.js
+++ p5js/keyviz/classes/keyviz_control_panel.js
@@ -12,16 +12,31 @@ class KeyvizControlPanel {
 
   initButtons(){
     let buttonWidth = 150;
-    this.uiSet.add( new Button(KeyvizControlPanel.BTN_START_RECORDING, "Start Recording", this.x, this.y + 0, buttonWidth) );
-    this.uiSet.add( new Button(KeyvizControlPanel.BTN_STOP_RECORDING, "Stop Recording", this.x, this.y + 50, buttonWidth) );
-    this.uiSet.add( new Button(KeyvizControlPanel.BTN_REPLAY_RECORDING, "Replay Recording", this.x, this.y + 100, buttonWidth) );
-    this.uiSet.add( new Button(KeyvizControlPanel.BTN_STOP_REPLAY, "Stop Replay", this.x, this.y + 150, buttonWidth) );
+    let buttonConfigs = this.configForButtons();
+
+    let buttonYPos = this.y;
+    buttonConfigs.forEach(btnConfig => {
+      let newButton = new Button(btnConfig.id, 
+                                 btnConfig.label, 
+                                 this.x, buttonYPos, buttonWidth);
+      this.uiSet.add(newButton);
+      buttonYPos += 50;
+    });
   }
 
   static get BTN_START_RECORDING() { return 0; }
   static get BTN_STOP_RECORDING() { return 1; }
   static get BTN_REPLAY_RECORDING() { return 2; }
   static get BTN_STOP_REPLAY() { return 3; }
+
+  configForButtons(){
+    return [
+      {id: KeyvizControlPanel.BTN_START_RECORDING, label: 'Start Recording'},
+      {id: KeyvizControlPanel.BTN_STOP_RECORDING, label: 'Stop Recording'},
+      {id: KeyvizControlPanel.BTN_REPLAY_RECORDING, label: 'Replay Recording'},
+      {id: KeyvizControlPanel.BTN_STOP_REPLAY, label: 'Stop Replay'}
+    ];
+  }
 
   handleMousePressed(){
     this.uiSet.handleMousePressed();
