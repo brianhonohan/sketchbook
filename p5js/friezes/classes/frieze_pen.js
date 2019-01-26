df4f3a08a01af10edb0f265685df7d27dbbf8d4f
Brian Honohan <brian.honohan@gmail.com>
2019-01-25 23:14:17 -0500

    p5js/friezes - Add basic repeating concept
    * This will calculate the number tiles dynamically, and repeatedly draw to them.

--- p5js/friezes/classes/frieze_pen.js
+++ p5js/friezes/classes/frieze_pen.js
@@ -1,11 +1,34 @@
 class FriezePen {
   constructor(sizeAndPos){
     this.area = sizeAndPos;
+    this.numCols = this.calcNumTilesWide();
+
+    this.pos = createVector(0, 0);
+    this.prevPos = createVector(0, 0);
+  }
+
+  get x(){ return this.area.x; }
+  get y(){ return this.area.y; }
+  get width(){ return this.area.width; }
+  get height(){ return this.area.height; }
+
+  calcNumTilesWide(){
+    const allowedWidth = width - 2 * this.x;
+    const numCols = Math.floor(allowedWidth / this.width);
+    return numCols;
   }
 
   drawRepeatedly(){
-    line(pmouseX, pmouseY, mouseX, mouseY);
-  } 
+    this.pos.set(mouseX, mouseY);
+    this.prevPos.set(pmouseX, pmouseY);
+
+    for (var i = 0; i < this.numCols; i++){
+      line(this.prevPos.x, this.prevPos.y, this.pos.x, this.pos.y);
+
+      this.pos.x += this.width;
+      this.prevPos.x += this.width;
+    }
+  }
 
   draw(){
     if (mouseIsPressed && this.area.containsXY(mouseX, mouseY)){
