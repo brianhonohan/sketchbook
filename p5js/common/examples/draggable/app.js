2c05024869617821680b86f3c7074a75ee0acde6
Brian Honohan <brian.honohan@gmail.com>
2019-02-03 23:42:00 -0500

    p5js/common-ex - Add rect that will soon be draggable

--- p5js/common/examples/draggable/app.js
+++ p5js/common/examples/draggable/app.js
@@ -1,10 +1,17 @@
 var system;
+var rectToDrag;
 
 function setup() {
   createCanvas(500, 500);
   P5JsSettings.init();
+
+  rectToDrag = new Rect(50, 50, 200, 200);
 }
 
 function draw(){
   background(50);
+
+  fill(80);
+  noStroke();
+  P5JsUtils.drawRect(rectToDrag);
 }
