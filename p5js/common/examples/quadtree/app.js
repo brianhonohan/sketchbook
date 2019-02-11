13f56ae22f3f6fa795dbdd29970826fac5301b2e
Brian Honohan <brian.honohan@gmail.com>
2019-02-10 22:32:55 -0500

    p5js/common - Implement basic Quadtree
    * Supports adding new objects, and expanding to accommodate new items.
    * Rendering takes place outside of the data object (to keep that light weight).
    
    Note: I wonder if its beneficial to keep the object array outside of the Quadtree and add the objects on each loop cycle
    ... alternative is to detect when an object moves from one quadrant to another.
    
    Reference: https://en.wikipedia.org/wiki/Quadtree

--- p5js/common/examples/quadtree/app.js
+++ p5js/common/examples/quadtree/app.js
@@ -1,16 +1,41 @@
 var canvas;
+var quadtree;
 
 function setup() {
   createCanvas(500, 500);
   P5JsSettings.init();
+
+  let fullCanvas = new Rect(0, 0, width, height);
+  quadtree = new Quadtree(fullCanvas, 5);
 }
 
 function draw(){
   background(50);
+
+  noFill();
+  stroke(0, 230, 0);
+  drawQuadtree(quadtree);
 }
 
 function keyTyped(){
   if (key == 'p'){
     saveCanvas(canvas, 'screenshot', 'png');
+  }
+}
+
+function mousePressed(){
+  quadtree.add({x: mouseX, y: mouseY});
+}
+
+function drawQuadtree(qt){
+  if (qt.expanded){
+    qt.quadrants.forEach(q => drawQuadtree(q));
+  } else {
+    strokeWeight(1)
+    qt.objects.forEach(obj => {
+      point(obj.x, obj.y);
+    });
+    strokeWeight(0.5);
+    rect(qt.area.x, qt.area.y, qt.area.width, qt.area.height);
   }
 }
