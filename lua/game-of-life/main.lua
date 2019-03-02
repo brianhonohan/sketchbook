7ec00d88930d3c56f1897966a5e92793fabbf9da
Brian Honohan <brian.honohan@gmail.com>
2019-03-02 18:05:38 -0500

    lua/game-of-life - Implement 'nextState'
    Follows the rules:
    
    1. Any live cell with fewer than two live neighbors dies, as if by underpopulation.
    2. Any live cell with two or three live neighbors lives on to the next generation.
    3. Any live cell with more than three live neighbors dies, as if by overpopulation.
    4. Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
    
    From: https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life#Rules

--- lua/game-of-life/main.lua
+++ lua/game-of-life/main.lua
@@ -1,10 +1,14 @@
 cellGrid = {}
-cellWidth = 20
+cellWidth = 5
 
 function love.load()
   print('hello')
   print("more info")
   initGrid(cellGrid, cellWidth)
+end
+
+function love.update()
+  stepGrid(cellGrid)
 end
 
 function love.draw()
@@ -57,4 +61,90 @@ end
 
 function colForIdx(grid, idx)
   return (idx-1) % grid.numCols
+end
+
+function stepGrid(grid)
+  local newData = {}
+  local tmpCount = 0
+
+  for i = 1,grid.numCells do
+    tmpCount = countSurrounding(grid, i)
+    newData[i] = nextState(grid.data[i], tmpCount)
+  end
+
+  grid.data = newData
+end
+
+function neighborsForIdx(grid, idx)
+  -- optimistically set them (ignoring edge cases)
+  local neighbors = {}
+  neighbors[1] = idx - grid.numCols - 1
+  neighbors[2] = idx - grid.numCols
+  neighbors[3] = idx - grid.numCols + 1
+  neighbors[4] = idx - 1
+  neighbors[5] = idx + 1
+  neighbors[6] = idx + grid.numCols - 1
+  neighbors[7] = idx + grid.numCols
+  neighbors[8] = idx + grid.numCols + 1
+
+  -- check for edge cases
+
+  -- first row
+  if (idx < grid.numCols) then
+    neighbors[1] = neighbors[1] + grid.numCells
+    neighbors[2] = neighbors[2] + grid.numCells
+    neighbors[3] = neighbors[3] + grid.numCells
+  end
+
+  -- if in left most column
+  if (idx % grid.numCols == 0) then
+    neighbors[1] = neighbors[1] + grid.numCols
+    neighbors[4] = neighbors[4] + grid.numCols
+    neighbors[6] = neighbors[6] + grid.numCols
+  end
+
+  -- if in right most column
+  if (idx % grid.numCols == (grid.numCols - 1)) then
+    neighbors[3] = neighbors[3] - grid.numCols
+    neighbors[5] = neighbors[5] - grid.numCols
+    neighbors[8] = neighbors[8] - grid.numCols
+  end
+
+  -- if in bottom row
+  if ((grid.numCells - idx) <= grid.numCols) then
+    neighbors[6] = neighbors[6] - grid.numCells
+    neighbors[7] = neighbors[7] - grid.numCells
+    neighbors[8] = neighbors[8] - grid.numCells
+  end
+
+  return neighbors
+end
+
+function countSurrounding(grid, idx)
+  local count = 0
+  local neighbors = neighborsForIdx(grid, idx)
+  local tmpNeighborIdx = 0
+
+  for i=1,8 do
+    tmpNeighborIdx = neighbors[i]
+    if (grid.data[tmpNeighborIdx]) then
+      count = count + 1
+    end
+  end
+
+  return count
+end
+
+function nextState(currentState, count)
+  if (currentState == true) then
+    if (count < 2) then
+      return false
+    elseif (count > 3) then
+      return false
+    else
+      return true
+    end
+  else
+    return (count == 3)
+  end
 end
