ef5761428d38dd3b3ca703549d598be2a679382e
Brian Honohan <brian.honohan@gmail.com>
2019-03-02 17:14:52 -0500

    lua/game-of-life - Base commit with random grid
    (first sketch in Lua, so going with something familiar)
    
    This establishes a grid of cells, that are initialized to true or false, and colored green or dark gray accordingly.
    
    Mostly learning Lua from:
    https://www.youtube.com/watch?v=HYYsedq2Ng4&list=PLS9MbmO_ssyBAc9wBC85_WG9aT88KGxH8&index=1

---
+++ lua/game-of-life/main.lua
@@ -0,0 +1,52 @@
+cellGrid = {}
+cellWidth = 20
+
+function love.load()
+  print('hello')
+  print("more info")
+  initGrid(cellGrid, cellWidth)
+end
+
+function love.draw()
+  drawGrid(cellGrid)
+end
+
+function love.quit()
+  print("Closing")
+end
+
+function initGrid(grid, _cellWidth)
+  grid.data = {}
+  grid.wrap = true
+  grid.numRows = math.floor( love.graphics.getHeight() / _cellWidth )
+  grid.numCols = math.floor( love.graphics.getWidth() / _cellWidth )
+  grid.numCells = grid.numRows * grid.numCols
+
+  for i = 1,grid.numCells do
+    grid.data[i] = math.random() > 0.5
+  end
+end
+
+function drawGrid(grid)
+  local x = 0
+  local y = 0
+  local row = 0
+  local col = 0
+  local cell
+
+  for i = 1,grid.numCells do
+    row = math.floor( (i-1) / grid.numCols )
+    col = (i-1) % grid.numCols
+    cell = grid.data[i]
+
+    if (cell) then
+      love.graphics.setColor(0.2, 0.8, 0.2)
+    else
+      love.graphics.setColor(0.2, 0.2, 0.2)
+    end
+
+    x = col * cellWidth
+    y = row * cellWidth
+    love.graphics.rectangle('fill', x, y, cellWidth, cellWidth)
+  end
+end
