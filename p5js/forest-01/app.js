bbb07f4b8e1955609639d806eccf1ce19d6be8d6
Brian Honohan <brian.honohan@gmail.com>
2019-06-27 12:00:18 -0400

    p5js/forest-01 - Add config for intiial tree count
    The number of trees that the forest starts with can now be controlled via the 'initial_trees' param.
    
    This was done because with only 1 tree, there was a good chance that it would be eaten by the herbivores.

--- p5js/forest-01/app.js
+++ p5js/forest-01/app.js
@@ -5,6 +5,7 @@ var systemParams = {
   foraging_rate: 0.6,
   seeds_per_tree: 2,
   seed_drop_dist: 70,
+  initial_trees: 10,
   paused: false,
   tree: {
     max_age: 200,
@@ -22,6 +23,7 @@ function setup() {
   gui.add(systemParams, 'foraging_rate').min(0.1).max(0.9).step(0.05);
   gui.add(systemParams, 'seeds_per_tree').min(1).max(20).step(1);
   gui.add(systemParams, 'seed_drop_dist').min(1).max(150).step(10);
+  gui.add(systemParams, 'initial_trees').min(1).max(50).step(1);
   gui.add(systemParams, "paused");
 
   let treeCfg = gui.addFolder('Tree Attributes');
