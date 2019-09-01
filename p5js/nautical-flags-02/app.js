fd8888befc4472303ed1b83383ebc9a16ac52f7a
Brian Honohan <brian.honohan@gmail.com>
2019-09-01 16:52:03 -0400

    p5js/nautical-flags-02 - Improve layout of Welcome screen for mobile device
    Dynamically adjusts the layout of some of the UI components to improve the layout on mobile screens.

--- p5js/nautical-flags-02/app.js
+++ p5js/nautical-flags-02/app.js
@@ -61,25 +61,35 @@ function showIntroScreen(){
   background(50);
   fill(230);
   textFont('Verdana');
-  textAlign(CENTER, CENTER);
-  const marginX = 0.2 * width;
-
-  textSize(32);
-  text('Welcome', marginX, 0.25 * height, width - 2 * marginX, height / 3);
+  textAlign(CENTER, TOP);
+  const marginX = (width > 667) ? 0.2 * width : 0.05 * width;
+  const lineHeight = 40;
+  let yPos =  0.15 * height;
+  let flagSize = 0.8 * width / (7 * 1.1);
 
   nfTypeset.push();
-  nfTypeset.textSize(0.1 * height);
-  nfTypeset.text('WELCOME', 0.1 * width, 0.25 * height, 0.8 * width);
+  nfTypeset.textSize(flagSize);
+  nfTypeset.text('WELCOME', 0.1 * width, yPos, 0.8 * width);
   nfTypeset.pop();
 
-  const instr1 = 'Type on your keyboard to see the corresponding nautical flag.';
-  const instr2 = 'Press SHIFT + P to download an image of what you\'ve typed.';
-  const instr3 = 'Click the SHARE button to get a shareable URL to send a message to friend.';
+  textSize(32);
+  yPos += 1.5 * flagSize;
+  text('Welcome', marginX, yPos, width - 2 * marginX, lineHeight);
 
-  let instructions = instr1 + "\n\n" + instr2 + "\n\n" + instr3;
+  const instr1 = 'Type to see the corresponding nautical flags.';
+  const instr2 = 'Press SHIFT + P to download an image of what you\'ve typed.';
+  const instr3 = 'Click the SHARE button to get a shareable link to send a message to friend.';
+
+  let lineSpacer = (width > 667) ? "\n\n" : "\n\n";
+  let instructions;
+  if (width > 667) {
+    instructions = instr1 + lineSpacer + instr2 + lineSpacer + instr3;
+  } else {
+    instructions = instr1 + lineSpacer + instr3;
+  }
   textSize(20);
-  const mainBlockY = height / 2 - 50;
-  const lineHeight = 40;
-  text(instructions, marginX, mainBlockY, width - 2 * marginX, mainBlockY);
+  yPos += (width > 667) ? lineHeight * 2 : lineHeight;
+  const mainBlockHeight = height - yPos;
+  text(instructions, marginX, yPos, width - 2 * marginX, mainBlockHeight);
 }
 
