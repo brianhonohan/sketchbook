5819aa31a1070e98caf7131bbbb324e6feb87f5a
Brian Honohan <brian.honohan@gmail.com>
2019-09-18 23:03:31 -0400

    p5js/seven-segment-display - Add basic display of the seven segments
    Renders the 7 segments, following the A-G designation on Wikipedia:
    https://en.wikipedia.org/wiki/Seven-segment_display#Concept_and_visual_structure

--- p5js/coding-challenges/seven-segment-display/classes/seven_segment_display.js
+++ p5js/coding-challenges/seven-segment-display/classes/seven_segment_display.js
@@ -1,3 +1,90 @@
 class SevenSegmentDisplay {
-  
+  constructor(sizeAndPosition){
+    this.sizeAndPosition = sizeAndPosition;
+    this.value = 0;
+    this.calcDimensions();
+    this.initColors();
+  }
+
+  get x(){ return this.sizeAndPosition.x; }
+  get y(){ return this.sizeAndPosition.y; }
+  get maxX(){ return this.sizeAndPosition.maxX; }
+  get maxY(){ return this.sizeAndPosition.maxY; }
+  get width(){ return this.sizeAndPosition.width; }
+  get height(){ return this.sizeAndPosition.height; }
+
+  calcDimensions(){
+    this.horizBarWidth  = this.width  * 0.8;
+    this.horizBarHeight = this.height * 0.1;
+    this.vertBarWidth   = this.width  * 0.1;
+    this.vertBarHeight  = this.height * 0.35;
+  }
+
+  initColors(){
+    this.colors = {
+      on: color(200, 50, 50),
+      off: color(40, 20, 20),
+    };
+  }
+
+  draw(){
+    noStroke();
+    this.drawSegmentA(true);
+    this.drawSegmentB(true);
+    this.drawSegmentC(true);
+    this.drawSegmentD(true);
+    this.drawSegmentE(true);
+    this.drawSegmentF(true);
+    this.drawSegmentG(true);
+  }
+
+  fillForStatus(status){
+    if (status) { 
+      fill(this.colors.on);
+    } else {
+      fill(this.colors.off);
+    }
+  }
+
+  drawSegmentA(status){
+    this.fillForStatus(status);
+    rect(this.x + this.vertBarWidth, this.y, 
+         this.horizBarWidth, this.horizBarHeight);
+  }
+
+  drawSegmentB(status){
+    this.fillForStatus(status);
+    rect(this.maxX - this.vertBarWidth, this.y + this.horizBarHeight, 
+         this.vertBarWidth, this.vertBarHeight);
+  }
+
+  drawSegmentC(status){
+    this.fillForStatus(status);
+    rect(this.maxX - this.vertBarWidth, this.maxY - this.vertBarHeight - this.horizBarHeight, 
+         this.vertBarWidth, this.vertBarHeight);
+  }
+
+  drawSegmentD(status){
+    this.fillForStatus(status);
+    rect(this.x + this.vertBarWidth, this.maxY - this.horizBarHeight, 
+         this.horizBarWidth, this.horizBarHeight);
+  }
+
+  drawSegmentE(status){
+    this.fillForStatus(status);
+    rect(this.x, this.maxY - this.vertBarHeight - this.horizBarHeight, 
+         this.vertBarWidth, this.vertBarHeight);
+  }
+
+  drawSegmentF(status){
+    this.fillForStatus(status);
+    rect(this.x, this.y + this.horizBarHeight, 
+         this.vertBarWidth, this.vertBarHeight);
+  }
+
+  drawSegmentG(status){
+    this.fillForStatus(status);
+    rect(this.x + this.vertBarWidth, this.y + this.vertBarHeight + this.horizBarHeight, 
+         this.horizBarWidth, this.horizBarHeight);
+  }
 }
--- p5js/coding-challenges/seven-segment-display/classes/system.js
+++ p5js/coding-challenges/seven-segment-display/classes/system.js
@@ -3,7 +3,20 @@ class System {
     this.sizeAndPosition = p_xSizeAndPos;
     this.optionsSet = new OptionsSet(this.optionsMetadata());
     this.settings = this.optionsSet.settings;
+
+
+    this.marginX = 0.2 * this.width;
+    this.marginY = 0.2 * this.height;
+    let digitPlacement = new Rect(this.x + this.marginX, this.y + this.marginY, 
+                                    this.width - 2 * this.marginX,
+                                    this.height - 2 * this.marginY);
+    this.sevenSegDisplay = new SevenSegmentDisplay(digitPlacement)
   }
+
+  get x(){ return this.sizeAndPosition.x; }
+  get y(){ return this.sizeAndPosition.y; }
+  get width(){ return this.sizeAndPosition.width; }
+  get height(){ return this.sizeAndPosition.height; }
 
   // Return a list of Options, specific to this sketch,
   // that can be tweaked via the URL parameters
@@ -19,10 +32,11 @@ class System {
   }
 
   tick(){
-    console.log("tock");
+    // console.log("tock");
   }
 
   render(){
-    
+    background(50);
+    this.sevenSegDisplay.draw();
   }
 }
