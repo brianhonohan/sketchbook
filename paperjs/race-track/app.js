b9b19e9d8c29708015ac8a687ce362e527e190e5
Brian Honohan <brian.honohan@gmail.com>
2019-10-17 21:55:28 -0400

    paperjs/race-track - Establish list of components
    Adds an array of 'components' that can each receive the tick() call on each frame.

--- paperjs/race-track/app.js
+++ paperjs/race-track/app.js
@@ -1,20 +1,23 @@
 var trackPen;
 var trainEngine;
+var components;
 
 function setupPaper(canvasId){
   paper.setup('myCanvas');
 
   trackPen = new TrackPen();
   paper.tool = trackPen.tool;
+  components = [];
 }
 
 
 function tick(event){
-  trainEngine.tick();
+  components.forEach(c => c.tick());
 }
 
 function startTrain(){
   trainEngine = new TrainEngine(trackPen.activePath);
+  components.push(trainEngine);
   paper.view.onFrame = tick;
 }
 
