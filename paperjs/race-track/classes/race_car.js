e2c29ca0e9ae307c072a04e6ea6f6b1344b7847f
Brian Honohan <brian.honohan@gmail.com>
2019-10-20 22:17:43 -0400

    paperjs/race-track - Add concept of tires and wear
    The race car now has a set of tires, that wear out as the car goes round the track.
    
    The tires are represented as a simple measure of their condition; and a number ranging from 1 (new) to 0 (fully degraded).
    
    Additionally, there is a simple modeling of the weight shift around the turns. The "outside" tire (relatively to the turning direction) suffers from a 0.6 wearing factor, where as the "inside" tire suffers a bit less, 0.4, because there is less weight beared by that tire.
    
    The paper.js getCurvatureAt(...) function is used to obtain the intensity of the curve and the direction:
    http://paperjs.org/reference/path/#getcurvatureat-offset
    
    There is no implication on the car's performance with lower grade tires.
    
    NOTE:
    - Might be a premature detail refinement to perform 4 samples of the curvature over each step.

--- paperjs/race-track/classes/race_car.js
+++ paperjs/race-track/classes/race_car.js
@@ -5,14 +5,106 @@ class RaceCar {
     this.speed = 4;
     this.shape = new paper.Shape.Circle(this.location(), 10);
     this.shape.fillColor = new paper.Color(0, 0.7, 0);
+
+    this.replaceTires();
+    this.initWearMatrix();
+    this.initTireView();
   }
+
+  replaceTires(){
+    this.tireHardness = 100;
+    this.tires = new Array(4).fill(1);
+  }
+
+  get tireFrontL() { return this.tires[0]; }
+  get tireFrontR() { return this.tires[1]; }
+  get tireRearL() {  return this.tires[2]; }
+  get tireRearR() {  return this.tires[3]; }
 
   location(){
     return this.path.getPointAt(this.distTraveled % this.path.length);
   }
 
+  initWearMatrix(){
+     // Negative Curvature, turning left, wears right tires more
+    // Positive Curvature, turning right, wears left tires more
+    this.wearMatrix = [
+      // [negativeCurve, neutralTurn,   positiveCurve]
+      // [leftTurn,      neutralTurn,   rightTurn]
+      [0.4,              0,             0.6],
+      [0.6,              0,             0.4],
+      [0.4,              0,             0.6],
+      [0.6,              0,             0.4],
+    ];
+  }
+
+  _wearFactor(tireIndex, curvature){
+    let curveIdx = 1 + Math.sign(curvature);
+    return this.wearMatrix[tireIndex][curveIdx] * Math.abs(curvature) / this.tireHardness;
+  }
+
+  _degradeTires(){
+    let samplePoints = this._samplePoints();
+    let curveSamples = samplePoints.map(sp => this.path.getCurvatureAt(sp));
+
+    this.tires.forEach((t, idx) => {
+      this.tires[idx] -= curveSamples.map(c => this._wearFactor(idx, c))
+                                     .reduce((wear, totalWear) => wear + totalWear);
+    });
+  }
+
+  _samplePoints(numSamples = 4){
+    let samples = new Array(numSamples).fill(null);
+    let stepSize = this.speed / numSamples;
+    let distOffset = this.distTraveled % this.path.length;
+    return samples.map((e, idx) => distOffset + idx * stepSize);
+  }
+
   tick(event){
+    this._degradeTires();
     this.distTraveled += this.speed;
     this.shape.position = this.location();
+
+    this._setTireViewColors();
+  }
+
+  initTireView(){
+    let screenWidth = paper.view.size.width;
+    let tireSize = new paper.Size(15, 25);
+
+    let margin = 20;
+
+    let baseX = screenWidth - 2 * tireSize.width - 2 * margin;
+    let x = baseX;
+    let y = margin;
+
+    this.tireViews = [];
+    this.tireViews.push( new paper.Shape.Rectangle(new paper.Point(x, y), tireSize) );
+
+    x += tireSize.width + margin;
+    this.tireViews.push( new paper.Shape.Rectangle(new paper.Point(x, y), tireSize) );
+
+    x = baseX;
+    y += tireSize.height + margin;
+    this.tireViews.push( new paper.Shape.Rectangle(new paper.Point(x, y), tireSize) );
+
+    x += tireSize.width + margin;
+    this.tireViews.push( new paper.Shape.Rectangle(new paper.Point(x, y), tireSize) );
+  }
+
+  _setTireViewColors(){
+    this.tireViews.forEach((tireView, idx) => {
+      tireView.fillColor = this.colorForTire(idx);
+    });
+  }
+
+  colorForTire(idx){
+    return this.colorForTireCondition(this.tires[idx]);
+  }
+
+  colorForTireCondition(condition){
+    let red   = 1 - condition;
+    let green = condition;
+    return new paper.Color(red, green, 0);
   }
 }
