8972e48d219570f980a0bac9d17a4288e1b427f4
Brian Honohan <brian.honohan@gmail.com>
2020-03-22 00:46:00 -0400

    paperjs/int-of-color - Add top-level page margin
    Create an internally managed margin, rather than CSS, to allow the sketch to reserve the ability to place *some* elements in this margin.

--- paperjs/interaction-of-color/app.js
+++ paperjs/interaction-of-color/app.js
@@ -6,7 +6,14 @@ var plateViewer;
 function setupPaper(canvasId){
   paper.setup('myCanvas');
 
-  let rect = new paper.Rectangle(0, 0, paper.view.size._width, paper.view.size._height);
+  let width = paper.view.size._width;
+  let height = paper.view.size._height;
+  let xMargin = 0.1 * width;
+  let yMargin = 0.1 * height;
+
+  let rect = new paper.Rectangle(xMargin, yMargin
+                                , width - 2 * xMargin
+                                , height - 2 * yMargin);
   plateViewer = new PlateViewer(rect);
   plateViewer.viewPlate(Plates.iv_1);
 
