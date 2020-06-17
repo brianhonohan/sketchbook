c86f206faa02eaee55c4128b25ce6097efe1d9f8
Brian Honohan <brian.honohan@gmail.com>
2020-06-17 00:08:16 -0400

    p5js/forest-02 - Add plot of age distribution of the forest
    Uses Vega Lite's `hconcat` to insert a second chart.
    
    See:
    https://vega.github.io/vega-lite/docs/concat.html

--- p5js/forest-02/classes/data_visualizer.js
+++ p5js/forest-02/classes/data_visualizer.js
@@ -13,39 +13,71 @@ class DataVisualizer {
   buildVlSpec(){
     return {
         $schema: 'https://vega.github.io/schema/vega-lite/v4.json',
-        height: 90,
-        width: 500,
-        data: {
-          name: 'population_v_time',
-          values: [
-             {time: -101, population: 200},  // hack to leave room for axis labels by giving large value, will be removed based on 'minimumX'
+        hconcat: [
+          {
+            width: 450,
+            height: 90,
+            data: {
+              name: 'population_v_time',
+              values: [
+                 {time: -101, population: 200},  // hack to leave room for axis labels by giving large value, will be removed based on 'minimumX'
 
-             // {time: -101, population: 200},
-             // {time: -101, population: 200},
-             // {time: -101, population: 200},
-             // {time: -101, population: 200},
-          ]
-        },
-        mark: {
-            type: "line",
-            interpolate: "step-after"
-        },
-        encoding: {
-          y: {
-            field: 'population',
-            type: 'quantitative',
-            axis: {
-              title: '# of Trees'
-            }
-        },
-          x: {
-            field: 'time',
-            type: 'quantitative',
-            axis: {
-              title: 'Time (seconds)'
-            }
+                 // {time: -101, population: 200},
+                 // {time: -101, population: 200},
+                 // {time: -101, population: 200},
+                 // {time: -101, population: 200},
+              ]
+            },
+            mark: {
+                type: "line",
+                interpolate: "step-after"
+            },
+            encoding: {
+              y: {
+                field: 'population',
+                type: 'quantitative',
+                axis: {
+                  title: '# of Trees'
+                }
+            },
+              x: {
+                field: 'time',
+                type: 'quantitative',
+                axis: {
+                  title: 'Time (seconds)'
+                }
+              }
+            },
+          },
+          {
+            width: 450,
+            height: 90,
+            data: {
+              name: 'age_distribution',
+              values: [
+                 // {age: 200, population: 200},  // hack to leave room for axis labels by giving large value, will be removed based on 'minimumX'
+              ]
+            },
+            mark: "bar",
+            encoding: {
+              y: {
+                aggregate: 'count',
+                type: 'quantitative',
+                axis: {
+                  title: '# of Trees'
+                }
+            },
+              x: {
+                field: 'age',
+                bin: true,
+                type: 'quantitative',
+                axis: {
+                  title: 'Age (years)'
+                }
+              }
+            },
           }
-        },
+        ],
         usermeta: {
             embedOptions: {
                 actions: false
@@ -70,11 +102,27 @@ class DataVisualizer {
           };
         };
 
+        function ageDistributionGenerator() {
+          var cycle = 0;
+
+          return function () {
+            cycle++;
+            return system.forest.trees.map(t => {
+              return {age: t.age, _cycle: cycle};
+            });
+          };
+        }
+
         var valueGenerator = populationVsTimeDataMapper();
         var minimumX = -100;
 
+        var ageDistGenerator = ageDistributionGenerator();
+        var updateCycle = 0;
+
         window.setInterval(function () {
           minimumX++;
+          updateCycle++;
+
           var changeSet = vega
             .changeset()
             .insert(valueGenerator())
@@ -82,6 +130,14 @@ class DataVisualizer {
               return t.time < minimumX;
             });
           res.view.change('population_v_time', changeSet).run();
+
+          var ageChangeSet = vega
+            .changeset()
+            .insert(ageDistGenerator())
+            .remove(function (t) {
+              return t._cycle == (updateCycle - 1);
+            });
+          res.view.change('age_distribution', ageChangeSet).run();
         }, 1000);
       });
   }
