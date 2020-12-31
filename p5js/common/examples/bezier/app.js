def11667eb8ebbce27c7f6cc456011377bfa3cfb
Brian Honohan <brian.honohan@gmail.com>
2020-12-30 21:08:09 -0500

    p5js/common - Add 'r' reset toggle to reinitialize location of shapes
    To reset the drawing quickly. (instead of reloading the page).

--- p5js/common/examples/bezier/app.js
+++ p5js/common/examples/bezier/app.js
@@ -6,37 +6,7 @@ var bezierCircle;
 function setup() {
   createCanvas(500, 500);
   P5JsSettings.init();
-
-  shapes = [];
-
-  bezierCurve = new BezierCurve(0.2 * width, 0.4 * height,
-                                0.4 * width, 0.4 * height,
-                                0.6 * width, 0.2 * height,
-                                0.8 * width, 0.2 * height);
-  bezierCurve.dragEnabled = true;
-  // bezierCurve.makeLinear();
-  // bezierCurve.move(150, 180);
-  shapes.push(bezierCurve);
-
-  polybezier = new Polybezier();
-  polybezier.append(new BezierCurve(0.2 * width, 0.5 * height,
-                                0.3 * width, 0.6 * height,
-                                0.3 * width, 0.8 * height,
-                                0.2 * width, 0.8 * height));
-  polybezier.append(new BezierCurve(100, 10,  // first point doesn't matter
-                                0.45 * width, 0.75 * height,
-                                0.55 * width, 0.75 * height,
-                                0.8 * width, 0.8 * height));
-  polybezier.append(new BezierCurve(10, 100,  // first point doesn't matter
-                                0.7 * width, 0.8 * height,
-                                0.7 * width, 0.7 * height,
-                                0.8 * width, 0.5 * height));
-  polybezier.close();
-  polybezier.dragEnabled = true;
-  shapes.push(polybezier);
-
-  bezierCircle = Polybezier.circle(0.7 * width, 0.5 * height, 0.15 * width);
-  shapes.push(bezierCircle);
+  resetShapes();
 }
 
 function draw(){
@@ -65,8 +35,44 @@ function mouseReleased(){
 function keyPressed(){
   switch(key){
     case 'd':
-      console.log("D pressed");
       shapes.forEach(s => s.toggleDragEnabled());
       break;
+    case 'r':
+      resetShapes();
+      break;
   }
+}
+
+function resetShapes(){
+  shapes = [];
+
+  bezierCurve = new BezierCurve(0.2 * width, 0.4 * height,
+                                0.4 * width, 0.4 * height,
+                                0.6 * width, 0.2 * height,
+                                0.8 * width, 0.2 * height);
+  bezierCurve.dragEnabled = true;
+  // bezierCurve.makeLinear();
+  // bezierCurve.move(150, 180);
+  shapes.push(bezierCurve);
+
+  polybezier = new Polybezier();
+  polybezier.append(new BezierCurve(0.2 * width, 0.5 * height,
+                                0.3 * width, 0.6 * height,
+                                0.3 * width, 0.8 * height,
+                                0.2 * width, 0.8 * height));
+  polybezier.append(new BezierCurve(100, 10,  // first point doesn't matter
+                                0.45 * width, 0.75 * height,
+                                0.55 * width, 0.75 * height,
+                                0.8 * width, 0.8 * height));
+  polybezier.append(new BezierCurve(10, 100,  // first point doesn't matter
+                                0.7 * width, 0.8 * height,
+                                0.7 * width, 0.7 * height,
+                                0.8 * width, 0.5 * height));
+  polybezier.close();
+  polybezier.smooth(Polybezier.SMOOTH_MODE_TRAILING);
+  polybezier.dragEnabled = true;
+  shapes.push(polybezier);
+
+  bezierCircle = Polybezier.circle(0.7 * width, 0.5 * height, 0.15 * width);
+  shapes.push(bezierCircle);
 }
