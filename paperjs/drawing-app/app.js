f239a8871be6291a6bfcbdd1c00d99cdfcbbe9f6
Brian Honohan <brian.honohan@gmail.com>
2023-01-22 13:13:47 -0500

    paperjs/drawing-app - Refactor tool set up to RootTool
    Centralize the management of tools in the RootTool object.

--- paperjs/drawing-app/app.js
+++ paperjs/drawing-app/app.js
@@ -1,14 +1,6 @@
-var tool;
-var path;
+var rootTool;
 
 function setupPaper(canvasId){
   paper.setup('myCanvas');
-  setupTools();
-}
-
-function setupTools(){
-  let rootTool = new RootTool(new paper.Tool(), null);
-  let lineTool = new LineTool(new paper.Tool(), rootTool);
-
-  lineTool.activate();
+  rootTool = new RootTool(new paper.Tool(), null);
 }
--- paperjs/drawing-app/js/tools/root_tool.js
+++ paperjs/drawing-app/js/tools/root_tool.js
@@ -1,5 +1,16 @@
 class RootTool extends BaseTool {
   constructor(tool, parentTool){
     super(tool, parentTool);
+    
+    this.setupTools();
+  }
+
+  setupTools(){
+    this.tools = {};
+
+    this.tools['root'] = this;
+    this.tools['line'] = new LineTool(new paper.Tool(), this);
+
+    this.tools['line'].activate();
   }
 }
