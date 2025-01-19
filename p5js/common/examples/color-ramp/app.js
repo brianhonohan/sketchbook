2b0bae79c351b43b681cc4a47c4466e30b21558c
Brian Honohan <brian.honohan@gmail.com>
2025-01-18 22:04:53 -0500

    p5js/common/color-ramp - add dat.gui controls

--- p5js/common/examples/color-ramp/app.js
+++ p5js/common/examples/color-ramp/app.js
@@ -1,19 +1,32 @@
 let colorRamp;
 let rampPos = {};
 
+const settings = {
+  color_ramp: 2,
+  bin_count: 20
+}
+
 function setup(){
-  // canvas = createCanvas(windowWidth, windowHeight-35);
-  canvas = createCanvas(500, 500);
+  canvas = createCanvas(windowWidth, windowHeight-35);
+  // canvas = createCanvas(500, 500);
   // colorRamp = P5jsColorRamp.elevation();
   // colorRamp = P5jsColorRamp.visibleSpectrum();
   colorRamp = P5jsColorRamp.temperatureScale();
+  colorRamp.setBinCount(settings.bin_count);
 
   rampPos.width = 0.1 * width;
   rampPos.height = 0.8 * height;
   rampPos.x = (width/2) - rampPos.width / 2;
   rampPos.y = (height/2) - rampPos.height / 2;
 
-  colorRamp.setBinCount(20);
+  // gui = P5JsSettings.addDatGui({autoPlace: false});
+  // guiColorRamp = gui.add(settings, "color_ramp", 1, 3, 1);
+  // guiBinCount = gui.add(settings, "bin_count", 5,40,1);
+  // addGuiListeners();
+}
+function addGuiListeners(){
+  guiColorRamp.onFinishChange((value) => pen.setNumberofSlices(value) )
+  guiBinCount.onFinishChange((_) => colorRamp.setBinCount(settings.bin_count) );
 }
 
 function draw(){
