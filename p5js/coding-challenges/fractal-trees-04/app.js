17b578efad909c8a475faf42ee0dcf5d3d356249
Brian Honohan <brian.honohan@gmail.com>
2025-03-21 23:27:58 -0400

    p5js/cc/fractal-trees-04 - Add beginnings of saved / predefined systems

--- p5js/coding-challenges/fractal-trees-04/app.js
+++ p5js/coding-challenges/fractal-trees-04/app.js
@@ -3,11 +3,34 @@ var systemViewer;
 var visualScale;
 var log10 = Math.log(10);
 var settings;
+let gui;
 
 const buttonFunc = {
   'step': stepFunc,
   'regenerate': regenerate
 };
+
+const predefinedSystems = {
+  'Default': {
+    axiom: 'F',
+    rule: 'FF[+F][-F]F',
+    angle: 45
+  },
+  'Koch Curve': {
+    axiom: 'F',
+    rule: 'F+F−F−F+F',
+    angle: 90
+  },
+  // 'Sierpinski Triangle': {
+  //   axiom: 'F-G-G',
+  //   rule: 'F-G+F+G-F'
+  // },
+  // 'Dragon Curve': {
+  //   axiom: 'FX',
+  //   rule: 'X+YF+',
+  //   angle: 90
+  // }
+}
 
 function setup(){
   createCanvas(windowWidth, windowHeight-40);
@@ -18,11 +41,12 @@ function setup(){
   settings = optionsSet.settings;
 
   gui = P5JsSettings.addDatGui({autoPlace: false});
+  gui.add(settings, "use_predefined", Object.keys(predefinedSystems)).onChange(loadPredefinedSystem);
   gui.add(settings, "axiom").onChange(regenerate);
   gui.add(settings, "rule").onChange(regenerate);
   gui.add(settings, "angle", 0, 180, 1);
   gui.add(settings, "segment_length", 0, 50, 1);
-  gui.add(settings, "auto_step", 0, 20, 1).onChange(regenerate);
+  gui.add(settings, "auto_step", 0, 6, 1).onChange(regenerate);
   gui.add(buttonFunc, "step");
   gui.add(buttonFunc, "regenerate");
 
@@ -39,6 +63,29 @@ function draw(){
   scale(visualScale);
   systemViewer.draw(system);
   pop();
+}
+
+function loadPredefinedSystem(){
+  const systemName = settings.use_predefined;
+  settings.axiom = predefinedSystems[systemName].axiom;
+  settings.rule = predefinedSystems[systemName].rule;
+  settings.angle = predefinedSystems[systemName].angle || settings.angle;
+
+  updateGui('axiom', settings.axiom);
+  updateGui('rule', settings.rule);
+  updateGui('angle', settings.angle);
+  updateGui('segment_length', settings.segment_length);
+  regenerate();
+}
+
+function updateGui(controlName, value){
+  for (const controller of gui.controllers){
+    if (controller.property === controlName){
+      controller.setValue(value);
+      controller.updateDisplay();
+      return;
+    }
+  }
 }
 
 function regenerate(){
@@ -75,6 +122,7 @@ function mouseWheel(event){
 function optionsMetadata(){
   return [
     // { name: "axiom", type: "integer", default: 0.2 * width},
+    { name: "use_predefined", type: "string", default: 'Default'},
     { name: "axiom", type: "string", default: 'F'},
     { name: "rule", type: "string", default: 'FF[+F][-F+F]F'},
     { name: "angle", type: "float", default: 45},
