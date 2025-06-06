ff37dba534b04b8d3cbdf59b4c06d147739824a9
Brian Honohan <brian.honohan@gmail.com>
2025-06-05 22:40:34 -0600

    p5js/cc/reaction-diffusion - Add control of the diffusion variables

--- p5js/coding-challenges/reaction-diffusion/app.js
+++ p5js/coding-challenges/reaction-diffusion/app.js
@@ -9,6 +9,15 @@ function setup() {
 
   let rect = new Rect(0, 0, width, height);
   system = new System(rect);
+
+  let reactDiffGui = gui.addFolder('Reaction Diffusion');
+  let reactDiff = system.reactionDiff;
+  reactDiffGui.add(reactDiff.settings, 'diffRateA', 0.0, 1.0, 0.01).name('Diffusion Rate A');
+  reactDiffGui.add(reactDiff.settings, 'diffRateB', 0.0, 1.0, 0.01).name('Diffusion Rate B');
+  reactDiffGui.add(reactDiff.settings, 'killRate', 0.0, 1.0, 0.001).name('Kill Rate');
+  reactDiffGui.add(reactDiff.settings, 'feedRate', 0.0, 1.0, 0.001).name('Feed Rate');
+  reactDiffGui.add(reactDiff.settings, 'dt', 0.0, 0.99, 0.01).name('Delta Time');
+
   background(255);
 }
 
@@ -17,6 +26,9 @@ function draw(){
   system.render();
 }
 
-function mouseDragged(){
+function mouseDragged(event){
+  if (event.target.nodeName != "CANVAS") {
+    return;
+  }
   system.addBAt(mouseX, mouseY);
 }
