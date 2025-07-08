249b9b73b9a904c6f9f298f50829605fdbe9a411
Brian Honohan <brian.honohan@gmail.com>
2025-07-07 21:59:21 -0600

    p5js/random - Clean up existing sketches; remove blendMode meta
    Didn't end up using this is these sketches and feel that it clutters the files.

--- p5js/random/019/app.js
+++ p5js/random/019/app.js
@@ -1,9 +1,6 @@
 var canvas;
 var gui;
 
-let blendModesDetails;
-let blendModeOptions;
-
 var systemParams = {
   xStepSize: 0.02,
   yStepSize: 0.07,
@@ -14,8 +11,6 @@ var systemParams = {
   hsbStart: 0,
   hsbEnd: 150,
   hsbCycle: false,
-  blendMode: 'BLEND',
-  blendModeIdx: 0,
   xStepFraction: 0.7
 };
 
@@ -23,8 +18,6 @@ function setup(){
   // canvas = createCanvas(500, 500);
   canvas = createAutosizedCanvas();
   colorMode(HSB);
-
-  initBlendModeOptions();
 
   gui = P5JsSettings.addDatGui({autoPlace: false});
   gui.add(systemParams, 'xStepSize', 0.001, 0.3, 0.001);
@@ -40,12 +33,10 @@ function setup(){
 }
 
 function draw(){
-  blendMode(BLEND);
   background(0, 0, 20);
 
   let yIncrement = systemParams.yStepSize * height;
 
-  // setBlendModeByFrame();
   if (systemParams.hsbCycle) {
     updateHSBStartEnd();
   }
@@ -70,51 +61,6 @@ function windowResized(event, noRedraw = false) {
   resizeCanvas(innerWidth, 
               innerHeight - drawingContext.canvas.getBoundingClientRect().top,
               noRedraw);
-}
-
-function initBlendModeOptions(){
-  blendModesDetails = {
-    'BLEND': {p5jsID: BLEND, description: 'color values from the source overwrite the canvas. This is the default mode.'},
-    'ADD': {p5jsID: ADD, description: 'color values from the source are added to values from the canvas.'},
-    // 'DARKEST': {p5jsID: DARKEST, description: 'keeps the darkest color value.'},
-    'LIGHTEST': {p5jsID: LIGHTEST, description: 'keeps the lightest color value.'},
-    'EXCLUSION': {p5jsID: EXCLUSION, description: 'similar to DIFFERENCE but with less contrast.'},
-    'MULTIPLY': {p5jsID: MULTIPLY, description: 'color values from the source are multiplied with values from the canvas. The result is always darker.'},
-    // 'SCREEN': {p5jsID: SCREEN, description: 'all color values are inverted, then multiplied, then inverted again. The result is always lighter. (Opposite of MULTIPLY)'},
-    // 'REPLACE': {p5jsID: REPLACE, description: 'the last source drawn completely replaces the rest of the canvas.'},
-    'REMOVE': {p5jsID: REMOVE, description: 'overlapping pixels are removed by making them completely transparent.'},
-    'DIFFERENCE': {p5jsID: DIFFERENCE, description: 'color values from the source are subtracted from the values from the canvas. If the difference is a negative number, it is made positive.'},
-    'OVERLAY': {p5jsID: OVERLAY, description: 'combines MULTIPLY and SCREEN. Dark values in the canvas get darker and light values get lighter.'},
-    'HARD_LIGHT': {p5jsID: HARD_LIGHT, description: 'combines MULTIPLY and SCREEN. Dark values in the source get darker and light values get lighter.'},
-    'SOFT_LIGHT': {p5jsID: SOFT_LIGHT, description: 'a softer version of HARD_LIGHT.'},
-    'DODGE': {p5jsID: DODGE, description: 'lightens light tones and increases contrast. Divides the canvas color values by the inverted color values from the source.'},
-    'BURN': {p5jsID: BURN, description: 'darkens dark tones and increases contrast. Divides the source color values by the inverted color values from the canvas, then inverts the result.'}
-  }
-  blendModeOptions = Object.keys(blendModesDetails);  
-}
-
-function setBlendModeByFrame(){
-  setBlendModeIdx(Math.floor(frameCount / 60) % blendModeOptions.length);
-}
-
-function incrementBlendMode(){
-  let newIdx = (systemParams.blendModeIdx + 1) % blendModeOptions.length;
-  setBlendModeIdx(newIdx);
-}
-
-function setBlendModeIdx(newIdx){
-  let isNewIdxValue = (newIdx != systemParams.blendModeIdx);
-  systemParams.blendModeIdx = newIdx;
-
-  let newBlendMode = blendModeOptions[systemParams.blendModeIdx];
-  systemParams.blendMode = newBlendMode;
-
-  // if (isNewIdxValue){
-    // console.log(`Switching to: ${systemParams.blendMode}`);
-  // }
-  
-  let blendModeRecord = blendModesDetails[systemParams.blendMode];
-  blendMode( blendModeRecord.p5jsID );
 }
 
 function updateHSBStartEnd(){
