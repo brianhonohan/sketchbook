b247927726770fad8d8df59eeb50a0d8e093cd4a
Brian Honohan <brian.honohan@gmail.com>
2025-08-12 21:19:52 -0600

    p5js/random - Add base sketch as copy/paste of random-020

---
+++ p5js/random/021/app.js
@@ -0,0 +1,107 @@
+let system;
+let canvas;
+let vertMargin = determineVerticalMargin();
+
+let gui;
+let modeOptions;
+
+function setup() {
+  // canvas = createCanvas(500, 500); // for screenshots
+  canvas = createCanvas(windowWidth, windowHeight-vertMargin);
+  P5JsSettings.init();
+
+  let rect = new Rect(0.1 * width, 0.1 * height, 0.8 * width, 0.8 * height);
+  system = new System(rect);
+
+  gui = P5JsSettings.addGui({autoPlace: false});
+  gui.add(system.settings, "mode", LayoutUtilFunctions.getPointModes()).onFinishChange(handleModeChange);
+  gui.add(system.settings, "num_points", 1, 3000, 1).onFinishChange(regenerate);
+  gui.add(system.settings, "homing_points").onFinishChange(regenerate);
+  handleModeChange();
+}
+
+// TODO: Move this dynamic UI mgmt into common GuiUtils
+function handleModeChange(){
+  removeControlersFromFolder('Mode Options');
+
+  let optionsForMode = LayoutUtilFunctions.getOptionsForMode(system.settings.mode);
+  if (optionsForMode.length == 0){
+    modeOptions = {}
+    regenerate();
+    return;
+  }
+
+  const folder = findOrCreateFolder('Mode Options');
+
+  modeOptions = {}
+  for(let i = 0; i < optionsForMode.length; i++){
+    let option = optionsForMode[i];
+    modeOptions[option.name] = option.default;
+
+    if (option.name == 'separationAngle' && option.maxValue == 360){
+      // Fixes breaking issue with voronoi generation
+      option.maxValue = 359.9999;
+    }
+    switch (option.type) {
+      case 'list':
+        folder.add(modeOptions, option.name, option.options).onFinishChange(regenerate);
+        break;  
+      case 'integer':
+        folder.add(modeOptions, option.name, option.minValue, option.maxValue).onChange(regenerate);
+        break;
+      case 'float':
+        folder.add(modeOptions, option.name, option.minValue, option.maxValue).onChange(regenerate);
+        break;
+    }
+  }
+  regenerate();
+}
+
+
+// TODO: Move this dynamic UI mgmt into common GuiUtils
+function findOrCreateFolder(folderName){
+  return findFolder(folderName) || gui.addFolder(folderName);
+}
+// TODO: Move this dynamic UI mgmt into common GuiUtils
+function findFolder(folderName){
+  for (let i = 0; i < gui.folders.length; i++){
+    if (gui.folders[i]._title === folderName){
+      return gui.folders[i];
+    }
+  }
+  return undefined;
+}
+// TODO: Move this dynamic UI mgmt into common GuiUtils
+function removeControlersFromFolder(folderName){
+  const folder = findFolder(folderName);
+  if (undefined == folder){
+    return;
+  }
+
+  const childControllers = folder.controllersRecursive();
+  for (let i = childControllers.length - 1; i >= 0; i--) {
+    childControllers[i].destroy();
+  }
+}
+
+function regenerate(){
+  system.regenerate(modeOptions)
+}
+
+function draw(){
+  system.tick();
+  system.render();
+}
+
+function determineVerticalMargin(){
+  let fullUrl = window.location.href;
+  return (fullUrl.indexOf(".html") > 0) ? 0 : 37;
+}
+
+function keyTyped(){
+  switch (key) {
+    case 'P':
+      saveCanvas(canvas, 'screenshot', 'png');
+      break;
+  }
+}
---
+++ p5js/random/021/classes/system.js
@@ -0,0 +1,107 @@
+class System {
+  constructor(p_xSizeAndPos){
+    this.area = p_xSizeAndPos;
+    this.optionsSet = new OptionsSet(this.optionsMetadata());
+    this.settings = this.optionsSet.settings;
+    this.points = [];
+ 
+    this.regenerate();
+  }
+
+  get x() { return this.area.x; }
+  get y() { return this.area.y; }
+  get width() { return this.area.width; }
+  get height() { return this.area.height; }
+
+  regenerate(modeOptions){
+    let relativeRect = new Rect(0, 0, this.area.width, this.area.height);
+    let relativeCircle = new Circle(this.area.width / 2,
+                                    this.area.height / 2,
+                                    Math.min(this.area.width, this.area.height) / 2);
+    let region = relativeRect;
+    
+    const layoutOptions = {};
+    layoutOptions.mode = this.settings.mode;
+
+    if (layoutOptions.mode == 'around-circle' 
+      || layoutOptions.mode == 'within-circle'
+      || layoutOptions.mode == 'spiral-fermat')
+    {
+      region = relativeCircle;
+    }
+
+    const locations = LayoutUtilFunctions.getPoints(layoutOptions.mode, region,
+                                               this.settings.num_points, modeOptions);
+    this.placePoints(locations);
+
+    // this.boundingRect = {xl: this.x, xr: this.x + this.width, yt: this.y, yb: this.y + this.height};
+    this.boundingRect = {xl: 0, xr: this.width, yt: 0, yb: this.height};
+    this.rebuildDiagram();
+  }
+
+  rebuildDiagram(){
+    this.voronoiDiag = createVoronoi(this.points, this.boundingRect);
+    this.voronoiDiag.fillColor = color(230);
+    this.fullRedraw = true;
+  }
+
+  placePoints(locations){
+    for (let i = 0; i < locations.length; i++){
+      if (this.points[i]){
+        if (this.settings.homing_points){
+          this.points[i].setTarget(new Point(locations[i][0], locations[i][1]));
+        } else {
+          this.points[i].x = locations[i][0];
+          this.points[i].y = locations[i][1];
+        }
+      }
+      else{
+        this.points.push(new HomingPoint(locations[i][0], locations[i][1]));
+      }
+    }
+    if (this.points.length > locations.length){
+      this.points = this.points.slice(0, locations.length);
+    }
+  }
+
+  // Return a list of Options, specific to this sketch,
+  // that can be tweaked via the URL parameters
+  // separate from the default P5JsSettings that are supported
+  //
+  // supported types: integer, float, string, bool
+  optionsMetadata(){
+    return [
+      { name: "num_points", type: "integer", default: 500},
+      { name: "mode", type: "String", default: 'spiral-fermat'},
+      { name: "homing_points", type: "bool", default: true},
+      
+      // { name: "varname2", type: "string", default: 'Lorem Ipsum'}, 
+      // { name: "varname3", type: "float", default: 0.6}
+      // { name: "varname4", type: "bool", default: false}
+    ];
+  }
+
+  tick(){
+    let movedOnePoint = false;
+    if (this.settings.homing_points){
+      let tmpPointMoved = false;
+      for (let i = 0; i < this.points.length; i++){
+        tmpPointMoved = this.points[i].update();
+        movedOnePoint ||= tmpPointMoved;
+      }
+    }
+    if (movedOnePoint){
+      this.rebuildDiagram();
+    }
+  }
+
+  render(){
+    if (this.fullRedraw){
+      background(50);
+      drawVoronoi(this.voronoiDiag, this.x, this.y);
+      this.fullRedraw = false;
+      return;
+    }
+    drawVoronoi(this.voronoiDiag, this.x, this.y, {redrawAll: false});
+  }
+}
